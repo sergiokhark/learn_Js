3ef943ffd5b239b4371cb6c443195cdcc6a4e1ea
sergiokhark <sergio.khark@gmail.com>
2022-06-18 17:01:07 +0300

    17.7_Objects

--- script.js
+++ script.js
@@ -1,3 +1,2 @@
-let obj = { key1: 1, key2: 2, key3: 3 };
-
-alert(obj.key1 + obj.key2 + obj.key3);
+let user = { name: 'Sergey', surname: 'Kamazin', patronymic: 'Sergeevich' };
+alert(user.surname + ' ' + user.name + ' ' + user.patronymic);
