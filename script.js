79bb004e0bb15f3c2b40e1496c570621d30c00dd
sergiokhark <sergio.khark@gmail.com>
2022-06-19 09:01:21 +0300

    23.3 If_else

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let arr = [1, 2, 3, 4, 5];
-let key1 = 1;
-let key2 = 2;
-alert(arr[key1] + arr[key2]);
+let num1 = 1;
+let num2 = 10;
+if (num1 <= 1 && num2 >= 3) {
+    alert('Верно');
+} else {
+    alert('Не верно');
+}
