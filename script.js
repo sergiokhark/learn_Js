e9526625db26f031dbb4d63db4da5b06bb326942
sergiokhark <sergio.khark@gmail.com>
2022-06-19 17:04:25 +0300

    32.1_Tern_oper

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-num = 3;
-switch (num) {
-    case 1:
-        alert('Зима');
-        break;
-    case 2:
-        alert('Весна');
-        break;
-    case 3:
-        alert('Лето');
-        break;
-    case 4:
-        alert('Осень');
-        break;
-    default:
-        alert('Ошибка');
-}
+let num = 15;
+let result = num >= 0 ? 1 : -1;
+alert(result);
