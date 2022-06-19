6203470dcba6aaa9e5dd8c6332fa9a76c85f6d6d
sergiokhark <sergio.khark@gmail.com>
2022-06-19 11:49:54 +0300

    27.1_If_else

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-let test = NaN;
-		
-if (test == false) {
-	alert('верно');
-} else {
-	alert('неверно');
+let day = 20;
+if (day >= 1 && day <= 10) {
+    alert('Первая декада')
+}
+if (day > 10 && day <= 20) {
+    alert('Вторая декада')
+}
+if (day > 20 && day <= 31) {
+    alert('Третья декада')
 }
