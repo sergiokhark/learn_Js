f7c35e65ffa266fce79aa013b6089c87de816635
sergiokhark <sergio.khark@gmail.com>
2022-06-19 15:55:12 +0300

    30.1_If_else

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-let arr = [12, 22, 44, 'b'];
-if (arr.length == 3) {
-    alert('В массиве три элемента');
-} else {
-    alert('В массиве не три элемента');
+let month = 11;
+if ((month >= 1 && month <= 2) || month == 12) {
+    alert('Зима');
+}
+if (month >= 3 && month <= 5) {
+    alert('Весна');
+}
+if (month >= 6 && month <= 8) {
+    alert('Лето');
+}
+if (month >= 9 && month <= 11) {
+    alert('Осень');
 }
