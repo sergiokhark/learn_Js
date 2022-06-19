4552cdfbb5450a211294ff3ae7209aacb6a69186
sergiokhark <sergio.khark@gmail.com>
2022-06-19 11:55:37 +0300

    27.3_If_else

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let day = 20;
+let day = 10;
 if (day >= 1 && day <= 10) {
     alert('Первая декада')
-}
-if (day > 10 && day <= 20) {
+} else if (day > 10 && day <= 20) {
     alert('Вторая декада')
-}
-if (day > 20 && day <= 31) {
+} else if (day > 20 && day <= 31) {
     alert('Третья декада')
+} else {
+    alert('Неправильный ввод')
 }
