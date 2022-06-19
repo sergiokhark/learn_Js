95b66f30cdf0db094deb2ff9989d674c067fd51b
sergiokhark <sergio.khark@gmail.com>
2022-06-19 16:10:29 +0300

    30.5_If_else

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-let num = 999;
+let num = 127226;
 let str = String(num);
-let sum = Number(str[0]) + Number(str[1]) + Number(str[2]);
-alert(sum);
+let sum1 = Number(str[0]) + Number(str[1]) + Number(str[2]);
+let sum2 = Number(str[3]) + Number(str[4]) + Number(str[5]);
+if (sum1 == sum2) {
+    alert('Yes');
+} else {
+    alert('No');
+}
