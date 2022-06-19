4a7edb9dcb30be12eba75055d2a2028f060a5439
sergiokhark <sergio.khark@gmail.com>
2022-06-19 16:02:26 +0300

    30.3_If_else

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let str = 'wabcde';
-if (str[0] == 'a') {
-    alert('Да');
+let num = 132345;
+let str = String(num);
+if (str[0] == 1 || str[0] == 2 || str[0] == 3) {
+    alert('Yes');
 } else {
-    alert('Нет');
+    alert('No');
 }
