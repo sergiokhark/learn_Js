f084558a368b76953c74e8679283926c73c48f5c
sergiokhark <sergio.khark@gmail.com>
2022-06-19 15:58:14 +0300

    30.2_If_else

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-let month = 11;
-if ((month >= 1 && month <= 2) || month == 12) {
-    alert('Зима');
-}
-if (month >= 3 && month <= 5) {
-    alert('Весна');
-}
-if (month >= 6 && month <= 8) {
-    alert('Лето');
-}
-if (month >= 9 && month <= 11) {
-    alert('Осень');
+let str = 'wabcde';
+if (str[0] == 'a') {
+    alert('Да');
+} else {
+    alert('Нет');
 }
