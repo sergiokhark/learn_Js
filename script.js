7205a5e64148ef3da8606e3e359a3f2023cec157
sergiokhark <sergio.khark@gmail.com>
2022-06-20 11:22:59 +0300

    39.7_For

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let arr = [2, 5, 9, 15, 1, 4];
-for (i = 0; i < arr.length; i++) {
-    if (arr[i] > 3 && arr[i] < 10) {
-        console.log(arr[i]);
+let summ = 0;
+let arr = [-12, -5, 9, -15, 1, 4, -22];
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+        summ += arr[i];
     }
 }
+console.log(summ);
