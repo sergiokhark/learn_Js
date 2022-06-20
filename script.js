422c5983deb96f77e0bcdfae96c01a1e1c149c50
sergiokhark <sergio.khark@gmail.com>
2022-06-20 11:12:14 +0300

    39.4_For

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
+let mult = 1;
 let arr = [1, 2, 3, 4, 5];
 for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 != 0) {
-        console.log(arr[i]);
-    }
+    mult *= arr[i];
 }
+console.log(mult);
