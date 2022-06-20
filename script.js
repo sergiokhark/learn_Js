5b4dcfbc608ac381b4e18b697cc85b25ba69caa1
sergiokhark <sergio.khark@gmail.com>
2022-06-20 11:07:12 +0300

    39.3_For

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let arr = ['a', 'b', 'c', 'd', 'e'];
+let arr = [1, 2, 3, 4, 5];
 for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
+    if (arr[i] % 2 != 0) {
+        console.log(arr[i]);
+    }
 }
