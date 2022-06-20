0c6dca34f7088be8dc310f5069cfab72a63d7761
sergiokhark <sergio.khark@gmail.com>
2022-06-20 11:15:01 +0300

    39.5_For

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let mult = 1;
+let summ = 0;
 let arr = [1, 2, 3, 4, 5];
 for (let i = 0; i < arr.length; i++) {
-    mult *= arr[i];
+    summ += arr[i] ** 2;
 }
-console.log(mult);
+console.log(summ);
