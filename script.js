93b4f5d5694fcebb81131ec6a3ac72fae15c8dac
sergiokhark <sergio.khark@gmail.com>
2022-06-20 12:30:50 +0300

    39.10_For

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 let arr = [10, 20, 30, 50, 235, 3000];
-let str;
 
-for (let i = 0; i < arr.length; i++) {
-    str = String(arr[i]);
-    if (str[0] == 1 || str[0] == 2 || str[0] == 5) {
-        console.log(arr[i]);
-    }
+for (let i = arr.length - 1; i >= 0; i--) {
+    console.log(arr[i])
 }
