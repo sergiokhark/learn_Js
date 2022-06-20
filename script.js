f6c232ffb67a74ebd9a95b5838e3495286dfd26b
sergiokhark <sergio.khark@gmail.com>
2022-06-20 12:40:46 +0300

    39.13_For

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let arr = [10, 2, 30, 50, 5, 3000, 7];
 
 for (let i = 0; i < arr.length; i++) {
-    if (arr[i] == i + 1) {
-        console.log(arr[i]);
-    }
+    document.write(arr[i] + '<br>');
+    
 }
