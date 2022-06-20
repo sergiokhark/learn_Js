c142d40f2ae4764eb70be062d312c78e5bb733da
sergiokhark <sergio.khark@gmail.com>
2022-06-20 12:59:17 +0300

    39.15_For

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 let arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+let day = 4;
 for (let i = 0; i < arr.length; i++) {
-    document.write('<b>' + arr[i] + '</b>' + '&nbsp');
+    if (i + 1 == day) {
+        document.write('<i>' + arr[i] + '</i>' + ' ');
+    } else {
+        document.write(arr[i] + ' ')
+    }
 }
