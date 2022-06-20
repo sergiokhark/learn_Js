1b4ea70a77dab8a48d8980e7664a9c5fcf190f6c
sergiokhark <sergio.khark@gmail.com>
2022-06-20 14:48:01 +0300

    40.4_For

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-arr = ['январь', 'февраль', 'март', 'апрель', 'май'];
-for (let elem of arr) {
-    console.log(elem);
+let arr = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь'];
+let month = 12;
+for (let i = 0; i < arr.length; i++) {
+    if (i + 1 == month) {
+        document.write('<i>' + arr[i] + '</i>' + '<br>');
+    } else {
+        document.write(arr[i] + '<br>');
+    }
 }
