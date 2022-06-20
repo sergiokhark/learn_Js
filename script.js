93ca4afa125bda8faca51f82877eccad8ca7ca1a
sergiokhark <sergio.khark@gmail.com>
2022-06-20 21:51:26 +0300

    44.4_While

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let arr = [1, 5, 7, 7, 0, 3, 18, 22];
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] == 3) {
-        console.log(i);
-        break
-    }
+let sum = 0;
+let count = 1;
+while (sum <= 100) {
+    sum += count;
+    count++;
 }
+console.log(count);
