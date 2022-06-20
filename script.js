c9e8addd7f66a31ee7ae9ab78baf776ab60f85d9
sergiokhark <sergio.khark@gmail.com>
2022-06-20 21:33:22 +0300

    44.2_For

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-let arr = [1, 5, 6, 7, 0, 15, 18, 22];
+let arr = [1, 5, 6, 7, 0, -1, 18, 22];
+let sum = 0;
 for (let elem of arr) {
-    if (elem != 0) {
-        console.log(elem);
+    if (elem >= 0) {
+        sum += elem;
     } else {
-        break
-    }
+        break;
+    } 
 }
+console.log(sum);
