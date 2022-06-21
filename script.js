2fd65e0ae2e01183dd06b8e1e421462eea3baefa
sergiokhark <sergio.khark@gmail.com>
2022-06-21 16:41:49 +0300

    57.7_Obj

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let obj = { 'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5 };
+let obj = {1: 'пн', 2: 'вт', 3: 'ср', 4: 'чт', 5: 'пт', 6: 'сб', 7: 'вс'};
+let res = {};
 for (let key in obj) {
-    obj[key] = obj[key] ** 2;
+    res[obj[key]] = key;
 }
-console.log(obj);
+console.log(res);
