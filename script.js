b32569b92f4cbf866e24f16f3a54f1119f2402be
sergiokhark <sergio.khark@gmail.com>
2022-06-21 12:26:38 +0300

    51.1_For

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let str = '';
-for (let i = -1; i >= -9; i--) {
-    str += i;
+for (let i = 10; i <= 1000; i++) {
+    let str = String(i);
+    if (Number(str[0]) + Number(str[1]) == 5) {
+        console.log(str);
+    }
 }
-console.log(str);
