40062be9e6ab496a8be1846dc29a0d1e340cc12c
sergiokhark <sergio.khark@gmail.com>
2022-06-21 20:08:09 +0300

    60.3_Arr

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let arr = [1, 2, 3, 4, 5];
-for (i = 0; i < arr.length - 1; i++) {
-    console.log(arr[i] + arr[i + 1]);
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (i = 2; i < arr.length; i++) {
+    console.log(arr[i-2], arr[i-1]);
 }
