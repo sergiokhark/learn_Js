e41b1525575b91e9c1236973dff4bd2ef3f49ec1
sergiokhark <sergio.khark@gmail.com>
2022-06-21 16:23:56 +0300

    57.3_Obj

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ let arr1 = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
 let arr2 = [1, 2, 3, 4, 5, 6, 7];
 let obj = {};
 for (let i = 0; i < arr1.length; i++) {
-    obj[arr1[i]] = arr2[i];
+    obj[arr2[i]] = arr1[i];
 }
 console.log(obj);
