0f8b237d9d1b616de48f138794e2c2e8d0192cb9
sergiokhark <sergio.khark@gmail.com>
2022-06-21 19:24:42 +0300

    59.2_Arr

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-let arr = ['a', 'b', 'c', 'd', 'e'];
-let res = {};
+let arr = [1, 2, 3, 2, 4, 3, 5, 6, 3, 2, 3];
+let counter_2 = 0;
+let counter_3 = 0;
 
 for (let i = 0; i < arr.length; i++) {
-    res[arr[i]] = i + 1;
+    if (arr[i] == 2) {
+        counter_2++;
+    }
+    if (arr[i] == 3) {
+        counter_3++
+    }
 }
-console.log(res);
+console.log(counter_2, counter_3);
