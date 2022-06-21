20cbebebe9dbc38e5d7ebee9c00fddf023b65280
sergiokhark <sergio.khark@gmail.com>
2022-06-21 17:42:34 +0300

    58.6_Obj

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-let obj = { 'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5 };
-let keys = [];
-let values = [];
+let obj = {
+	1: 125,
+	2: 225,
+	3: 128,
+	4: 356,
+	5: 145,
+	6: 281,
+	7: 452,
+};
+let arr = [];
 for (let key in obj) {
-    keys.push(key);
-    values.push(obj[key]);
+    let str = String(obj[key]);
+    if (str[0] == 1 || str[0] == 2) {
+        arr.push(obj[key]);
+    }
 }
-console.log(keys);
-console.log(values);
+console.log(arr);
