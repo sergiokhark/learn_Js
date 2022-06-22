2d4adb3edd99a0afe3c2d3297224b7314839c9fc
sergiokhark <sergio.khark@gmail.com>
2022-06-22 07:08:44 +0300

    61.2_Fibo

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let one = 0;
 let two = 1;
+let three = 2;
 for (i = 1; i <= 10; i++) {
-    let current = one + two;
+    let current = one + two + three;
     one = two;
-    two = current;
+    two = three;
+    three = current;
     console.log(current);
 }
