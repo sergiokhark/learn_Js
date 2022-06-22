4287d0da904b862dcc83e49d86877849cbb923b6
sergiokhark <sergio.khark@gmail.com>
2022-06-22 10:20:56 +0300

    66.3.2_Arr

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-let arr = [[1, 2, 3], [4, 5], [6]];
+let arr = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
 let total = 0;
 for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr[i].length; j++) {
-        total += arr[i][j];
+        for (let k = 0; k < arr[i][j].length; k++) {
+            total += arr[i][j][k];
+        }
     }
 }
 console.log(total);
