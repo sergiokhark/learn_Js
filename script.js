194cd26b364267266cae8b99de0491e39fad22bb
sergiokhark <sergio.khark@gmail.com>
2022-06-22 07:52:06 +0300

    64.1_Pract

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-let arr = [];
-for (let i = 1; i <= 5; i++) {
-    let str = '';
-    for (let j = 0; j < 5; j++) {
-        str += i
-        
-    }
-    arr.push(str);
+for (i = 1; i <= 100; i++) {
+    document.write(i, '<br>');
 }
-console.log(arr);
