edf80745ac3d0628cd2576efda94da32b6eb4764
sergiokhark <sergio.khark@gmail.com>
2022-06-22 08:16:33 +0300

    64.10_Pract

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let arr = [1, 2, 3];
+let arr = [1, 2, 3, 8, 12];
 total = 0;
 for (let elem of arr) {
-    total += elem ** 2;
+    total += elem;
 }
-console.log(total);
+console.log(total / arr.length);
