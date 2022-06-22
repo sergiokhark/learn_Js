42809acc071306ca6d6b9c605ddbcbd9301dccdd
sergiokhark <sergio.khark@gmail.com>
2022-06-22 08:05:08 +0300

    64.6_Pract

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let arr = [];
-for (i = 1; i <= 10; i++) {
-    arr.push(i);
+let arr = [21, 9, 33, -8, 5, -18, 1, -1];
+for (let elem of arr) {
+    if (elem > 0 && elem < 10) {
+        console.log(elem);
+    }
 }
-console.log(arr);
