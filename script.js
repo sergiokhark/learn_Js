5fe9ccf16e8bb42fc247c636c62f19f51bbca79b
sergiokhark <sergio.khark@gmail.com>
2022-06-22 15:51:58 +0300

    71.2_Obj

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-let employees = [
-	{
-		name: 'name1',
-		salary: 300,
-	},
-	{
-		name: 'name2',
-		salary: 400,
-	},
-	{
-		name: 'name3',
-		salary: 500,
-	},
-];
-console.log(employees[0].salary + employees[1].salary);
+let students = {
+	'boys':  ['Коля', 'Вася', 'Петя'],
+	'girls': ['Даша', 'Маша', 'Лена'],
+};
+console.log(students['boys'][1], students['girls'][2]);
