3db5c3f9dbf0ac0686a7ac2f1dae7389d94d0b9c
sergiokhark <sergio.khark@gmail.com>
2022-06-22 14:03:10 +0300

    67.17_Arr

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+ //[[[1, 2], [3, 4]], [[5, 6], [7, 8]]]
+
 let arr = [];
-for (let i = 0, k = 2; i < 4; i++) {
+for (let i = 0, s = 1; i < 2; i++) {
     arr[i] = [];
-    for (let j = 0; j < 3; j++, k += 2) {
-        arr[i].push(k);
+    for (let j = 0; j < 2; j++) {
+        arr[i][j] = [];
+        for (let k = 0; k < 2; k++, s++) {
+            arr[i][j].push(s);
+        }
     }
 }
 console.log(arr);
