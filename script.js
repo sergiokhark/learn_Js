480945ab3355ddb03675701d19748c0eea0193a3
sergiokhark <sergio.khark@gmail.com>
2022-06-22 09:18:10 +0300

    65.2_Arr

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-let arr = [
-    ['a', 'b', 'c'],
-    ['d', 'e', 'f'],
-    ['g', 'h', 'i'],
-    ['j', 'k', 'l'],
-];
-console.log(arr[3][2], arr[1][1], arr[2][0], arr[0][0]);
+let arr = [[1, 2], [3, 4], [5, 6]];
+let total = arr[0][0] + arr[0][1] + arr[1][0] + arr[1][1] + arr[2][0] + arr[2][1];
+console.log(total);
