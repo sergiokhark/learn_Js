1d7ddf0bf8a388f44ac3ec60f01137a17bc865fb
sergiokhark <sergio.khark@gmail.com>
2022-06-22 07:12:41 +0300

    62.1_For

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-let one = 0;
-let two = 1;
-let three = 2;
-for (i = 1; i <= 10; i++) {
-    let current = one + two + three;
-    one = two;
-    two = three;
-    three = current;
-    console.log(current);
+let str = '';
+for (i = 0; i < 5; i++) {
+    str += 'xx';
+    document.write(str + '<br>');
 }
