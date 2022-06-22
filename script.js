52021bf958881444173570ec6e923b6d1ed52e96
sergiokhark <sergio.khark@gmail.com>
2022-06-22 13:21:07 +0300

    67.1_Arr

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-let arr = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
-let total = 0;
-for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-        for (let k = 0; k < arr[i][j].length; k++) {
-            total += arr[i][j][k];
-        }
+// [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5]]
+
+let arr = [];
+for (let i = 0; i < 3; i++) {
+    let subArr = [];
+    for (let j = 1; j <= 5; j++) {
+        subArr.push(j);
     }
+    arr.push(subArr);
 }
-console.log(total);
+console.log(arr);
