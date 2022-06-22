18cadf9b3b7d7c435444935272af9ed8940eb6b9
sergiokhark <sergio.khark@gmail.com>
2022-06-22 07:49:23 +0300

    63.2_Arr

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let arr = [];
-let str = '';
-for (let i = 0; i < 5; i++) {
-    str += 'xx';
+for (let i = 1; i <= 5; i++) {
+    let str = '';
+    for (let j = 0; j < 5; j++) {
+        str += i
+        
+    }
     arr.push(str);
 }
 console.log(arr);
