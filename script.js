aa3a85da8634977f9f44cc92087741c4042c4b56
sergiokhark <sergio.khark@gmail.com>
2022-06-22 08:09:17 +0300

    64.7_Pract

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let arr = [21, 9, 33, -8, 5, -18, 1, -1];
+let flag = false;
 for (let elem of arr) {
-    if (elem > 0 && elem < 10) {
-        console.log(elem);
+    if (elem == 5) {
+        flag = true;
+        break
     }
 }
+console.log(flag);
