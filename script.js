ddebe19a5290bbe8aa29895611cba1ce86d5da05
sergiokhark <sergio.khark@gmail.com>
2022-06-22 13:34:14 +0300

    67.3_Arr

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-//[['x', 'x', 'x', 'x'], ['x', 'x', 'x', 'x'], ['x', 'x', 'x', 'x']]
-
 let arr = [];
 for (let i = 0; i < 3; i++) {
     arr[i] = [];
-    for (let j = 0; j < 4; j++) {
-        arr[i][j] = 'x';
+    for (let j = 0; j < 2; j++) {
+        arr[i][j] = [];
+        for (let k = 0; k < 5; k++) {
+            arr[i][j][k] = k + 1;
+        }
     }
 }
 console.log(arr);
