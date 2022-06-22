89e35adbea9c5e869a2a55bf675ca8b92a1f303f
sergiokhark <sergio.khark@gmail.com>
2022-06-22 17:47:38 +0300

    72.3_Arr

--- script.js
+++ script.js
@@ -2,18 +2,33 @@ let employees = [
 	{
 		name: 'name1',
 		salary: 300,
+		age: 28,
 	},
 	{
 		name: 'name2',
 		salary: 400,
+		age: 29,
 	},
 	{
 		name: 'name3',
 		salary: 500,
+		age: 30,
+	},
+	{
+		name: 'name4',
+		salary: 600,
+		age: 31,
+	},
+	{
+		name: 'name5',
+		salary: 700,
+		age: 32,
 	},
 ];
-total = 0;
+let total = 0;
 for (let elem of employees) {
-    total += elem.salary
+    if (elem.age >= 30) {
+        total += elem.salary;
+    }
 }
 console.log(total);
