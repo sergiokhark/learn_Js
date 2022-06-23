b9e55bb523d61eb6365c61991767e9a4572577aa
sergiokhark <sergio.khark@gmail.com>
2022-06-23 10:45:02 +0300

    73.8_Math

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getRandomInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-console.log(getRandomInt(1, 100));
+let arr = [];
+for (let i = 0; i < 10; i++) {
+	arr.push(getRandomInt(1, 100));
+}
+console.log(arr);
