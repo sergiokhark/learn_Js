800870804a7cbc335ca9246b094153d6fac4b0d4
sergiokhark <sergio.khark@gmail.com>
2022-06-23 12:14:25 +0300

    74.5_Str

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let str = 'я учу javascript!';
-console.log(str.substr(2, 3), str.substr(6, 10));
-console.log(str.substring(2, 5), str.substring(6, 16));
-console.log(str.slice(2, 5), str.slice(6, -1));
+let str = 'abcde';
+if (str.indexOf('a') === -1) {
+	console.log('No');
+} else {
+	console.log('Yes');
+}
