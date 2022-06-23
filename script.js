e7f77d991fc35abf02396a4d86b06b8f1a16b144
sergiokhark <sergio.khark@gmail.com>
2022-06-23 16:49:54 +0300

    75.10_Arr

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//[1, 'a', 'b', 2, 3, 4, 'c', 5, 'e']
+
 let arr = [1, 2, 3, 4, 5];
-arr.splice(3, 0, 'a', 'b', 'c');
+arr.splice(1, 0, 'a', 'b');
+arr.splice(-1, 0, 'c')
+arr.push('e');
 console.log(arr);
