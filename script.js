f960b9a0c211ba0ccf69565289e8898f892d675b
sergiokhark <sergio.khark@gmail.com>
2022-06-23 07:44:53 +0300

    72.7_Arr_Obj

--- script.js
+++ script.js
@@ -1,32 +1,51 @@
 let data = [
 	{
-		1: 'data11',
-		2: 'data12',
-		3: 'data13',
-		4: 'data14',
+		1: [
+			'data111',
+			'data112',
+			'data113',
+		],
+		2: [
+			'data121',
+			'data122',
+			'data123',
+		],
 	},
 	{
-		1: 'data21',
-		2: 'data22',
-        3: 'data33',
-        4: 'data33',
+		1: [
+			'data211',
+			'data212',
+			'data213',
+		],
+		2: [
+			'data221',
+			'data222',
+			'data223',
+			'data224',
+		],
+		3: [
+			'data231',
+			'data232',
+			'data233',
+			'data234',
+			'data235',
+		],
 	},
 	{
-		1: 'data31',
-		2: 'data32',
-    },
-    {
-		1: 'data31',
-		2: 'data32',
-	},
-	{
-		1: 'data31',
-		2: 'data32',
+		1: [
+			'data411',
+			'data412',
+			'data413',
+		],
+		2: [
+			'data421',
+		],
 	},
 ];
 for (let elem of data) {
 	for (let key in elem) {
-		document.write(elem[key] + '<br>');
+		for (subElem of elem[key]) {
+			document.write(subElem + '<br>')
+		}
 	}
-}
-   
+}  
