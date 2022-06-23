42571636a97df561bb9e3bdafedd07db7e65f665
sergiokhark <sergio.khark@gmail.com>
2022-06-23 10:13:35 +0300

    73.3_Math

--- script.js
+++ script.js
@@ -1,51 +1,6 @@
-let data = [
-	{
-		1: [
-			'data111',
-			'data112',
-			'data113',
-		],
-		2: [
-			'data121',
-			'data122',
-			'data123',
-		],
-	},
-	{
-		1: [
-			'data211',
-			'data212',
-			'data213',
-		],
-		2: [
-			'data221',
-			'data222',
-			'data223',
-			'data224',
-		],
-		3: [
-			'data231',
-			'data232',
-			'data233',
-			'data234',
-			'data235',
-		],
-	},
-	{
-		1: [
-			'data411',
-			'data412',
-			'data413',
-		],
-		2: [
-			'data421',
-		],
-	},
-];
-for (let elem of data) {
-	for (let key in elem) {
-		for (subElem of elem[key]) {
-			document.write(subElem + '<br>')
-		}
-	}
-}  
+let arr = [4, 2, 5, 19, 13, 0, 10];
+let total = 0;
+for (let elem of arr) {
+	total += Math.pow(elem, 3);
+}
+console.log(Math.sqrt(total));
