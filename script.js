aa89a51ada405f43c01a9d93287494313cb830a2
sergiokhark <sergio.khark@gmail.com>
2022-06-23 17:34:35 +0300

    76.5_Pract

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-let str = 'word1 word2 word3';
-let arr = str.split(' ');
+let str = 'var_test_text_tt';
+let arr = str.split('_');
 for (i = 0; i < arr.length; i++) {
     arr[i] = arr[i].slice(0, 1).toUpperCase() + arr[i].slice(1);
 }
-let res = arr.join(' ');
+let res = arr.join('');
 console.log(res);
-
