3590b76ccc155c534242d069dac4bb6841846784
sergiokhark <sergio.khark@gmail.com>
2022-06-23 16:53:00 +0300

    75.11_Arr

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-//[1, 'a', 'b', 2, 3, 4, 'c', 5, 'e']
-
 let arr = [1, 2, 3, 4, 5];
-arr.splice(1, 0, 'a', 'b');
-arr.splice(-1, 0, 'c')
-arr.push('e');
-console.log(arr);
+let pos = arr.indexOf(3);
+if (pos != -1) {
+    console.log('Yes');
+} else {
+    console.log('No');
+}
