9761311d832a2072fd6d8ce35eef7496bce06acf
sergiokhark <sergio.khark@gmail.com>
2022-06-24 11:56:15 +0300

    80.7_Func

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function getArr(num) {
+function getArr(num1, num2) {
     let arr = [];
-    for (let i = 1; i <= num; i++) {
+    for (let i = num1; i <= num2; i++) {
         arr.push(i);
     }
     return arr;
 }
-console.log(getArr(15));
+console.log(getArr(10, 18));
