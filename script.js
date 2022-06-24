83730e37b222e37934b7c77a346bc0361bfb322c
sergiokhark <sergio.khark@gmail.com>
2022-06-24 11:54:27 +0300

    80.6_Func

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-function delElem(elem, arr) {
-   
-    let index = arr.indexOf(elem);
-    while (index != -1) {
-        arr.splice(index, 1);
-        index = arr.indexOf(elem, index);
+function getArr(num) {
+    let arr = [];
+    for (let i = 1; i <= num; i++) {
+        arr.push(i);
     }
     return arr;
 }
-console.log(delElem(1, [10, 1, 2, 3, 10, 11, 22, 1, 1, 1, 1, 1, 25, 1]));
+console.log(getArr(15));
