b1ad2c50793c9d1b98066dbbce89efc4fb423d90
sergiokhark <sergio.khark@gmail.com>
2022-06-24 11:44:02 +0300

    80.5_Func

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-function reverseStr(str) {
-    str = str.split('').reverse().join('');
-    return str;
+function delElem(elem, arr) {
+   
+    let index = arr.indexOf(elem);
+    while (index != -1) {
+        arr.splice(index, 1);
+        index = arr.indexOf(elem, index);
+    }
+    return arr;
 }
-console.log(reverseStr('переворот'));
+console.log(delElem(1, [10, 1, 2, 3, 10, 11, 22, 1, 1, 1, 1, 1, 25, 1]));
