89d061888df07b7059eca370f64a63ac8ff440c5
sergiokhark <sergio.khark@gmail.com>
2022-06-24 08:07:53 +0300

    78.4_Func

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function totalNum(num1, num2, num3) {
     console.log(num1 + num2 + num3);
 }
+let param1 = 10;
+let param2 = 2;
+let param3 = 3;
 
-totalNum(10, 5, 15);
-totalNum(-5, 5, 4);
+totalNum(param1, param2, param3);
