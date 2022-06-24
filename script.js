ba006ec96c6aa15d24b6b2d11753435648adbd42
sergiokhark <sergio.khark@gmail.com>
2022-06-24 09:33:39 +0300

    79.10_Func

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-function sqrt(num) {
-	return Math.sqrt(num);
+let i = 1;
+function func(num) {
+    while (true) {
+        num /= 2;
+        if (num < 10) {
+            return i;
+        }
+        i++;
+    }
 }
 
-function sum(num1, num2, num3) {
-	return num1 + num2 + num3;
-}
-let result = sum(sqrt(2), sqrt(3), sqrt(4));
-console.log(result);
+console.log(func(46));
