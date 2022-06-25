09393c15340907ca7a3c67e8c3ff39f65519d6b9
sergiokhark <sergio.khark@gmail.com>
2022-06-25 15:57:13 +0300

    88.1_Func

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-function inArray(elem, arr) {
-    return arr.indexOf(elem) != -1
+function getDiff(arr1, arr2) {
+    
+    let firstDiff = getFirstDiff(arr1, arr2);
+    let secondDiff = getFirstDiff(arr2, arr1);
+
+    return [].concat(firstDiff, secondDiff);
 }
-function getInt(arr1, arr2) {
+
+function inArray(elem, arr) {
+    return arr.indexOf(elem) !== -1;
+}
+
+function getFirstDiff(arr1, arr2) {
     let result = [];
     for (let elem of arr1) {
-        if (inArray(elem, arr2)) {
+        if (!inArray(elem, arr2)) {
             result.push(elem);
         }
     }
     return result;
 }
-console.log(getInt([1, 8, 9, 11, 12, 18, 44, 5], [13, 15, 11, 5, 1, 88, 18]));
+console.log(getDiff([1, 2, 3], [2, 3, 4, 5]));
