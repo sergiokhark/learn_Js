5fcd03369813caba99a9d03510d41f00e050aa9b
sergiokhark <sergio.khark@gmail.com>
2022-06-25 16:39:35 +0300

    89.1_Func

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-function getDiff(arr1, arr2) {
-    
-    let firstDiff = getFirstDiff(arr1, arr2);
-    let secondDiff = getFirstDiff(arr2, arr1);
-
-    return [].concat(firstDiff, secondDiff);
+function getDivisors(num) {
+    let arr = [];
+    for (let i = 1; i <= num; i++) {
+        if (num % i == 0) {
+            arr.push(i);
+        }
+    }
+    return arr;
 }
 
 function inArray(elem, arr) {
     return arr.indexOf(elem) !== -1;
 }
 
-function getFirstDiff(arr1, arr2) {
+function getInt(arr1, arr2) {
     let result = [];
     for (let elem of arr1) {
-        if (!inArray(elem, arr2)) {
+        if (inArray(elem, arr2)) {
             result.push(elem);
         }
     }
     return result;
 }
-console.log(getDiff([1, 2, 3], [2, 3, 4, 5]));
+
+function getGreatestCommonDivisor(num1, num2) {
+    let intArr = getInt(getDivisors(num1), getDivisors(num2));
+    let NOD = Math.max.apply(null, intArr);
+    return NOD;
+}
+
+console.log(getGreatestCommonDivisor(180, 150));
