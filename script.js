ffb703d8bb42431472995de48c491920d2148920
sergiokhark <sergio.khark@gmail.com>
2022-06-26 15:06:40 +0300

    103.2_Func

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function test(func1, func2, func3) {
-    alert(func1() + func2() + func3());
+let f1 = function () {
+    return 5;
+};
+let f2 = function () {
+    return 10;
+};
+let f3 = function () {
+    return 15;
 }
 
-test(function func1() { return 1; }, function func2() { return 2; }, function func3() { return 3; });
+function test(func1, func2, func3) {
+    let result = func1() + func2() + func3();
+    return result;
+}
+
+console.log(test(f1, f2, f3));
