5eeb08e0eee3e7ee363f06c500c87e246608e5a8
sergiokhark <sergio.khark@gmail.com>
2022-06-26 16:07:07 +0300

    103.8_Func

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function test(func) {
-	alert(func(3));
+	alert(func(2, 3));
 }
 
-function func(num) {
-    return num ** 3;
+func1 = function func(num1, num2) {
+    return num1 + num2;
 }
 
-test(func);
+test(func1);
