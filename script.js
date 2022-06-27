475f0942bf0c318c932cade9edf75d1b618c567f
sergiokhark <sergio.khark@gmail.com>
2022-06-27 13:23:51 +0300

    107.4_Func

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function func(num1) {
     return function (num2) {
         return function (num3) {
-            return num1 + num2 + num3;
+            return function (num4) {
+                return function () {
+                    return [num1, num2, num3, num4];
+                }
+            }
         }
     }
 }
 
 
 
-alert(func(2)(3)(4));
+console.log(func(2)(3)(4)(5)());
