9608173d5fe2ed2c06426df9c954ed79cde18954
sergiokhark <sergio.khark@gmail.com>
2022-06-27 21:29:48 +0300

    111.1_Func

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-(function (num1) {
-    return function (num2) {
-        return function (num3) {
-            alert(num1 + num2 + num3);
-        }
-    }
-})(1)(2)(3);
+let func = (function () {
+	let num = 1;
+	return function () {
+		alert(num);
+		num++
+	}
+})();
+
+func();
+func();
+func();
