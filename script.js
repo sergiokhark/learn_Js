1e1c53523e149df3896b014c8786d8a2e4fdfe74
sergiokhark <sergio.khark@gmail.com>
2022-06-27 18:13:23 +0300

    110.14_Func

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 (function (num1) {
     return function (num2) {
-        alert(num1 + num2);
+        return function (num3) {
+            alert(num1 + num2 + num3);
+        }
     }
-})(1)(2);
+})(1)(2)(3);
