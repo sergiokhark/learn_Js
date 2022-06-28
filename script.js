bc0f3eb0e621d2f0cfaf88ed584937b8ca6e49c2
sergiokhark <sergio.khark@gmail.com>
2022-06-28 08:28:32 +0300

    112.3_Func

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function each(arr, callback) {
 	let result = [];
-	for (let elem of arr) {
-		result.push(callback(elem));
+	for (let i = 0; i < arr.length; i++) {
+		result.push(callback(arr[i], i));
 	}
 	return result;
 }
 
-let res = each(['строка1', 'строка2', 'строка3'], function (str) {
-	let str1 = str[0].toUpperCase() + str.slice(1);
-	return str1;
-})
+let res = each([2, 12, 5, 4], function (elem, numElem) {
+	let res = elem * numElem;
+	return res;
+});
 
 console.log(res);
