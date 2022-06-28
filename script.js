75af824f82ca4880f9b321e38303016053d1f567
sergiokhark <sergio.khark@gmail.com>
2022-06-28 20:43:51 +0300

    120.2_Meth

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let arr1 = [-1, -2, -5, -9];
-let arr2 = arr1.some(elem => elem > 0);
-console.log(arr2);
+let arr1 = [1, 2, 5, 11];
+let res = arr1.some((elem, index) => elem * index > 30);
+console.log(res);
 
