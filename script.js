69be4b231863bd831ef2ead3b33422314da14e55
sergiokhark <sergio.khark@gmail.com>
2022-06-28 20:03:20 +0300

    118.5_Meth

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-let arr = ['abcdefg', 'abc', 'vb', 'abbcbcbc'];
-let arr1 = arr.filter(elem => elem.length > 5);
+let arr = [2, 6, 16, 8, 22, 1, 39];
+let arr1 = arr.filter(function (elem, index) {
+	return elem * index < 30;
+});
 console.log(arr1);
