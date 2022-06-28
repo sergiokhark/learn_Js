665e23f68dd0510f28757ecb70e717f45014ed2b
sergiokhark <sergio.khark@gmail.com>
2022-06-28 09:16:51 +0300

    112.8_Func

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 function every(arr, callback) {
-	for (let elem of arr) {	
-		if (!callback(elem)) {
-			return false;
+	for (let elem of arr) {
+		if (callback(elem)) {
+			return true;
 		}
 	}
-	return true;
+	return false;
 }
 
-let result = every([1, 2, 3, 4, -5], function (elem) {
-	if (elem > 0) {
+let result = every([1, 2, 3, 4, 5], function (elem) {
+	if (elem < 0) {
 		return true;
 	} else {
 		return false;
-	};
+	}
 });
+
 console.log(result);
