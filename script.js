31d0903210bb80486d5ec3100a82338e062a5b46
sergiokhark <sergio.khark@gmail.com>
2022-06-28 16:07:41 +0300

    115.3_Func

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function func(obj) {
-	let sum = 0;
-	for (let key in obj) {
-		if (typeof obj[key] === 'object') {
-			sum += func(obj[key]);
+function strFun(arr) {
+	let str = '';
+	for (let elem of arr) {
+		if (typeof elem === 'object') {
+			str += strFun(elem);
 		} else {
-			sum += obj[key];
+			str += elem;
 		}
 	}
-	return sum;
+	return str;
 }
-console.log(func({a: 1, b: {c: 2, d: 3, e: 4}, f: {g: 5, j: 6, k: {l: 7, m: {n: 8, o: 9}}}}));
+console.log(strFun(['a', ['b', 'c', 'd'], ['e', 'f', ['g', ['j', 'k']]]]));
