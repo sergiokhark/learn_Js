b49fce5d0e7c2c5949fd64768be60ef340f2df6b
sergiokhark <sergio.khark@gmail.com>
2022-06-28 16:39:54 +0300

    115.5_Func

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-function func(arr) {
-	for (let i = 0; i < arr.length; i++) {
-		if (typeof arr[i] === 'object') {
-			arr[i] = func(arr[i]);
+let result = [];
+function getArray(arr) {
+	
+	for (let elem of arr) {
+		if (typeof elem === 'object') {
+			result.concat(getArray(elem));
 		} else {
-			arr[i] = arr[i] ** 2;
+			result.push(elem);
 		}
 	}
-	return arr;
+	return result;
 }
-console.log(func([1, [2, 7, 8], [3, 4], [5, [6, 7]]]));
+console.log(getArray([1, [2, 7, 8], [3, 4, [5, [6, 7]]]]));
