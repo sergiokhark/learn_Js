b61c785223f98a755fa1e1e76382d41d95ba98ae
sergiokhark <sergio.khark@gmail.com>
2022-06-28 19:12:20 +0300

    116.2_Meth

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let array = [4, 16, 25, 36, 20];
+let array = ['str1', 'str2', 'str3', 'str4'];
 let arr = array.map(function (elem) {
-	return Math.sqrt(elem);
+	return elem + '!'
 })
 console.log(arr);
