2258ffd006a9a7e13ea75ad49792183fd5fc393c
sergiokhark <sergio.khark@gmail.com>
2022-06-28 19:09:10 +0300

    116.1_Meth

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-let result = [];
-function getArray(arr) {
-	
-	for (let elem of arr) {
-		if (typeof elem === 'object') {
-			result.concat(getArray(elem));
-		} else {
-			result.push(elem);
-		}
-	}
-	return result;
-}
-console.log(getArray([1, [2, 7, 8], [3, 4, [5, [6, 7]]]]));
+let array = [4, 16, 25, 36, 20];
+let arr = array.map(function (elem) {
+	return Math.sqrt(elem);
+})
+console.log(arr);
