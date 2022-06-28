6ca25108c4a82cb8da9b5710057ea215d2091df0
sergiokhark <sergio.khark@gmail.com>
2022-06-28 20:14:41 +0300

    118.7_Meth

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-let arr1 = [1, 2, [3, 4], 5, [6, 7]];
-let arr2 = arr1.filter(elem => !Array.isArray(elem));
-console.log(arr2);
+let arr = [-1, 2, 3, -4, -5];
+let arr1 = arr.filter(elem => elem < 0);
+console.log(arr1.length);
+
+
