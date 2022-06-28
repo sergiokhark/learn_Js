c2e3d3429f6fd400ecee250d108cecdff389d57f
sergiokhark <sergio.khark@gmail.com>
2022-06-28 19:41:07 +0300

    117.1_Meth

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let arr = ['123', '456', '789'];
-let arr1 = arr.map(function (elem, index) {
-	return elem * index;
-})
-console.log(arr1);
+let arr = [2, 3, 1];
+let sum = 0;
+arr.forEach(function (elem) {
+	sum += elem ** 2;
+});
+console.log(sum);
