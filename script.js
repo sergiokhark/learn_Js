bf0f4a2a6c2280b5f5759d1ed1216c8f448bb8c9
sergiokhark <sergio.khark@gmail.com>
2022-06-28 15:19:40 +0300

    114.3_Func

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-let arr = [];
-function func(prevPrevNum, prevNum) {
-	let num = prevPrevNum + prevNum;
-	arr.push(prevPrevNum);
-	prevPrevNum = prevNum;
-	prevNum = num;
-	if (arr.length < 10) {
-		func(prevPrevNum, prevNum);	
-	}
-	return arr;
+function getDigitSum(num) {
+	return getSum(getDigit(num));
 }
 
-console.log(func(1, 2));
+function getDigit(num) {
+	return String(num).split('');
+} 
+
+function getSum(arr) {
+	let sum = 0;
+	for (let elem of arr) {
+		sum += Number(elem);
+	}
+	return sum;
+}
+
+function reduceNum(num) {
+	let sum = getDigitSum(num);
+	if (sum <= 9) {
+		return sum;
+	} else {
+		return reduceNum(sum);
+	}
+}
+
+console.log(reduceNum(2187));
