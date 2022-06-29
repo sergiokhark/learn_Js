2c93ece6f81725f584213e94a3fd373ba1d3b6e9
sergiokhark <sergio.khark@gmail.com>
2022-06-29 19:02:51 +0300

    127.1_???

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function getDiff(...arrs) {
     let result = [];
     for (let arr of arrs) {
         for (let elem of arr) {
+            elem = arr.shift();
             if (inArrs(elem, arrs)) {
                 result.push(elem);
             }
@@ -13,18 +14,15 @@ function getDiff(...arrs) {
 function inArrs(elem, arrs) {
     for (let arr of arrs) {
         if (inArr(elem, arr)) {
-            return true;        // true если элемента нет в массиве
-        } else {
-            return false;
+            return false;        // false если элемент есть в массиве
         }
     }
+    return true;
 }
 
 function inArr(elem, arr) {
-    return arr.indexOf(elem) === -1; // true если элемента нет в массиве
+    return arr.indexOf(elem) !== -1; // true если элемент есть в массиве
 }
 
 
-console.log(getDiff([2, 3, 11], [2, 3, 7], [4, 3, 21])); // [7, 4, 21]
-console.log(getDiff([18, 3, 11], [2, 3, 7], [4, 3, 21])); // [2, 7, 4, 21]
-console.log(getDiff([18, 3, 11], [7, 3, 7], [4, 3, 21])); // [7, 7, 4, 21]
+console.log(getDiff([1, 2, 3], [2, 3, 7], [4, 3]));
