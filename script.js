2a6275b3716c9a05d67da1c6e71eca20fc7b34b1
sergiokhark <sergio.khark@gmail.com>
2022-06-29 10:19:15 +0300

    125.3_Spread

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function getUnion(arr1, arr2, arr3) {
-    let res = [];
-    res.push(arr1, arr2, arr3);
-    return res;
+/* function getUnion(arr1, arr2, arr3) {
+    let result = arr1.concat(arr2, arr3);
+    return result;
 }
-console.log(getUnion([1, 2, 3], [4, 5, 6], [7, 8, 9]));
+console.log(getUnion([1, 2, 3], [4, 5, 6], [7, 8, 9])); */
+
+function getUnion(...arrs) {
+    return [].concat(...arrs);
+}
+console.log(getUnion([1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 2, 3], [4, 5, 6], [7, 8, 9]));
