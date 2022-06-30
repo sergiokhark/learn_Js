90c06f5fd768e77ca87d3d8b24a2152320d7931f
sergiokhark <sergio.khark@gmail.com>
2022-06-30 14:19:27 +0300

    132.1_Date

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-let date = new Date();
+//'12:59:59 31.12.2014'
 
-console.log(date.getDate());        // current day
-console.log(date.getMonth() + 1);   // current month
-console.log(date.getFullYear());    // current year
+function zeroNum(num) {
+    if (num >= 0 && num <= 9) {
+        return '0' + num;
+    } else {
+        return num;
+    }
+}
+
+
+let date = new Date();
+console.log(zeroNum(date.getHours()) + ':' + zeroNum(date.getMinutes()) + ':' + zeroNum(date.getSeconds()) +
+    ' ' + zeroNum(date.getDate()) + '.' + zeroNum(date.getMonth() + 1) + '.' + date.getFullYear());
