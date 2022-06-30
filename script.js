e8fa69cf65cf07724324a0b4d5b156709392f62f
sergiokhark <sergio.khark@gmail.com>
2022-06-30 13:30:12 +0300

    130.4_Destruct

--- script.js
+++ script.js
@@ -1,34 +1,11 @@
-function getDiff(...arrs) {
-    let result = [];
-    for (let i = 0; i < arrs.length; i++) {
-        let arr0 = arrs.pop();
-        for (let elem of arr0) {
-            if (checkInArrs(elem, arrs)) {
-                result.push(elem);
-            }
-        }
-        arrs.unshift(arr0);
-    }
-    return result;
+function func1(str) {
+    let arr = str.split('-');
+    return arr;
 }
 
-function checkInArrs(elem, arrs) {
-    let flag = true;                // элемент не повторяется
-    for (let arr of arrs) {
-        for (let subEl of arr) {
-            if (inArr(elem, arr)) {
-                flag = false;
-                break;
-            }
-        } 
-    }
-    return flag;
+function func(department, [name, surname], str) {
+    let [year, month, day] = func1(str);
+    console.log(department);
 }
 
-function inArr(elem, arr) {
-    return arr.indexOf(elem) !== -1; // true если элемент есть в массиве
-}
-
-
-
-console.log(getDiff([1, 6, 2, 3, 5], [4, 1, 2, 3], [12, 7, 1, 2, 3]));
+func('отдел разработки', ['Иван', 'Иванов'], '2018-12-31');
