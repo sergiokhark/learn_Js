ab7c992f10073cd358009c1dc08b4901df7de181
sergiokhark <sergio.khark@gmail.com>
2022-06-30 20:22:55 +0300

    142.1_Date

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 let now = new Date();
-let currentYear = now.getFullYear();
-let counter = 0;
-for (let year = 2000; year <= currentYear; year++) {
-    let date = new Date(year, 0, 1);
-    if (date.getDay() == 0 || date.getDay() == 6) {
-        counter++;
-    }
-}
-console.log(counter);
+let date = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+let diff = now - date;
+console.log(diff / 1000);
