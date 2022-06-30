71e86eb0dd4b90d5bb123912b9042292c0288f4a
sergiokhark <sergio.khark@gmail.com>
2022-06-30 21:08:35 +0300

    142.9_Date

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-let now = new Date();
-let date = new Date(now.getFullYear(), now.getMonth() + 1, 0);
-console.log(date.getDay());
+let date = new Date();
+let currentYear = date.getFullYear();
+let result;
+
+function visYear(year) {
+    let date = new Date(year, 2, 0);
+    return date.getDate() == 29;   // true если год високосный
+        
+}
+
+while (!visYear(currentYear)) { // пока год не високосный
+    
+    result = currentYear - 1;
+    currentYear--;
+}
+
+console.log(result);
+
+
