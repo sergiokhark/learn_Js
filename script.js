2262bc513b600fd44e24c9e9156fccad400880b8
sergiokhark <sergio.khark@gmail.com>
2022-06-30 20:43:09 +0300

    142.6_Date

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 let now = new Date();
-let date = new Date(now.getFullYear(), now.getMonth() - 3, now.getDate());
-console.log(date.getFullYear());
+let date = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+console.log(date.getDay());
