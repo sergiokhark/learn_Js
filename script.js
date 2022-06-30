0707a7510f309bd110d83d838b788d167f840b27
sergiokhark <sergio.khark@gmail.com>
2022-06-30 20:06:19 +0300

    141.13_Date

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-function checkDate(year, month, day) {
-    let date = new Date(year, month, day);
-    return date.getFullYear() == year && date.getMonth() == month && date.getDate() == day;
+let now = new Date();
+let currentYear = now.getFullYear();
+let counter = 0;
+for (let year = 2000; year <= currentYear; year++) {
+    let date = new Date(year, 0, 1);
+    if (date.getDay() == 0 || date.getDay() == 6) {
+        counter++;
+    }
 }
-
-
-
-console.log(checkDate(2025, 12, 31)); // выведет true
-console.log(checkDate(2025, 0, 3)); // выведет false
+console.log(counter);
