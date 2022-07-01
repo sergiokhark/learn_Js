af739abf23b30816e849ec83290aa14e55b61a3c
sergiokhark <sergio.khark@gmail.com>
2022-07-01 10:01:56 +0300

    145.3_Date

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 let now = new Date();
-let date = new Date(now.getFullYear(), 5, 24);
-let dateNewYear = new Date(now.getFullYear() + 1, 5, 24);
+let currentYear = now.getFullYear();
+let visYear;
 
-if (date - now > 0) {
-    console.log('До Дня Рождения осталось дней: ', (date - now) / (1000 * 60 * 60 * 24));
+function nextVisYear(year) {
+    let date = new Date(year, 2, 0);
+    return date.getDate() == 29;                    //true если год високосный
 }
-if (date == now) {
-    console.log('День рождения сегодня');
+
+function happyBirthDay(month, day) {
+    let date = new Date(now.getFullYear(), month, day, now.getHours(), now.getMinutes(), now.getSeconds(), now.getMilliseconds());
+    let nextDate = new Date(now.getFullYear() + 1, month, day, now.getHours(), now.getMinutes(), now.getSeconds(), now.getMilliseconds());
+
+    if (date - now > 0) {
+        console.log('До дня рождения осталось дней: ', Math.round((date - now) / (1000 * 60 * 60 * 24)));
+    };
+
+    if (now - date == 0) {                          // now == date не работает ?
+        console.log('День рождения сегодня');
+    };
+
+    if (date - now < 0 && !(month == 1 && day == 29)) {
+        console.log('До дня рождения осталось дней: ', Math.round((nextDate - now) / (1000 * 60 * 60 * 24)));
+    };
+
+    while (!nextVisYear(currentYear)) {            // пока год не високосный
+        visYear = currentYear + 1;
+        currentYear++;
+    }
+
+    let nextDate29 = new Date(visYear, 1, 29, now.getHours(), now.getMinutes(), now.getSeconds(), now.getMilliseconds());
+    
+    if (month == 1 && day == 29) {
+        console.log('До дня рождения 29 февраля осталось дней: ', Math.round((nextDate29 - now) / (1000 * 60 * 60 * 24)));
+    }
 }
-if (date - now < 0) {
-    console.log('День рождения в этом году уже был. До следующего осталось дней: ',
-        (dateNewYear - now) / (1000 * 60 * 60 * 24));
-}
+
+happyBirthDay(1, 29);
+
+
+
+
