05b1f46a98cf2b5716429048671d9bba33f4383a
sergiokhark <sergio.khark@gmail.com>
2022-07-01 18:43:33 +0300

    155.4_DOM

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let link = document.querySelector('#elem1');
 let button = document.querySelector('#button');
-let elem = document.querySelector('#elem2');
 
 button.addEventListener('click', function () {
-    elem.innerHTML = link.href;
+    link.innerHTML = link.innerHTML + '(' + link.href + ')';
 })
