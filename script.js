0ce1d491e6f3836e5e9cedff06d354fa3bd4b4a7
sergiokhark <sergio.khark@gmail.com>
2022-07-01 14:48:26 +0300

    152.1_DOM

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-let button1 = document.querySelector('#button1');
-button1.addEventListener('dblclick', function () { alert('Вы сделали двойной клик'); });
+function func() {
+	alert('message');
+}
+
+let elem1 = document.querySelector('#elem1');
+let elem2 = document.querySelector('#elem2');
+let elem3 = document.querySelector('#elem3');
+let elem4 = document.querySelector('#elem4');
+let elem5 = document.querySelector('#elem5');
+
+elem1.addEventListener('click', func);
+elem2.addEventListener('click', func);
+elem3.addEventListener('click', func);
+elem4.addEventListener('click', func);
+elem5.addEventListener('click', func);
