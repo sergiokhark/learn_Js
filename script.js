0673c39cc876e90cf991f27e5ded41cb3cd6bbf5
sergiokhark <sergio.khark@gmail.com>
2022-07-01 15:49:18 +0300

    154.8_DOM

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let elem1 = document.querySelector('#elem1');
 let button = document.querySelector('#button');
 button.addEventListener('click', function () {
-    elem1.innerHTML = Number(elem1.innerHTML) ** 2;
+    elem1.innerHTML = '!' + elem1.innerHTML;
 })
 
