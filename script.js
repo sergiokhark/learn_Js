6ff0a9bd520cc75360ee88e8d0fda2d89636fcd3
sergiokhark <sergio.khark@gmail.com>
2022-07-01 14:55:29 +0300

    153.2_DOM

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-function func() {
-	alert('message');
-}
+let elem = document.querySelector('#elem');
+elem.addEventListener('dblclick', function () {
+    alert(1);
+});
 
-let elem1 = document.querySelector('#elem1');
-let elem2 = document.querySelector('#elem2');
-let elem3 = document.querySelector('#elem3');
-let elem4 = document.querySelector('#elem4');
-let elem5 = document.querySelector('#elem5');
+elem.addEventListener('dblclick', function () {
+    alert(2);
+});
 
-elem1.addEventListener('click', func);
-elem2.addEventListener('click', func);
-elem3.addEventListener('click', func);
-elem4.addEventListener('click', func);
-elem5.addEventListener('click', func);
+elem.addEventListener('dblclick', function () {
+    alert(3);
+});
