f7ce68e856fcd9205686e8e54eeb7f0ab6928efc
sergiokhark <sergio.khark@gmail.com>
2022-07-01 15:21:26 +0300

    154.1_DOM

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-let elem = document.querySelector('#elem');
-elem.addEventListener('dblclick', function () {
-    alert(1);
-});
-
-elem.addEventListener('dblclick', function () {
-    alert(2);
-});
-
-elem.addEventListener('dblclick', function () {
-    alert(3);
+let elem1 = document.querySelector('#elem1');
+let elem2 = document.querySelector('#elem2');
+let button = document.querySelector('#button');
+button.addEventListener('click', function () {
+    alert(Number(elem1.innerHTML) + Number(elem2.innerHTML));
 });
