5338b1eeaf774f14245fc1ae5ff84027d1ef8787
sergiokhark <sergio.khark@gmail.com>
2022-07-01 08:26:02 +0300

    145.2_Date

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let now = new Date();
-let date = new Date(now.getFullYear(), 2, 8);
-let dateNewYear = new Date(now.getFullYear() + 1, 2, 8);
+let date = new Date(now.getFullYear(), 5, 24);
+let dateNewYear = new Date(now.getFullYear() + 1, 5, 24);
+
 if (date - now > 0) {
-    console.log('До праздника 8 марта осталось дней: ', (date - now) / (1000 * 60 * 60 * 24));
+    console.log('До Дня Рождения осталось дней: ', (date - now) / (1000 * 60 * 60 * 24));
 }
 if (date == now) {
-    console.log('Праздник 8 марта сегодня');
+    console.log('День рождения сегодня');
 }
 if (date - now < 0) {
-    console.log('Праздник 8 марта в этом году уже был. До следующего осталось дней: ',
+    console.log('День рождения в этом году уже был. До следующего осталось дней: ',
         (dateNewYear - now) / (1000 * 60 * 60 * 24));
 }
