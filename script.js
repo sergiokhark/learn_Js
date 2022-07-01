0225915e39df6934f2072f00471728b01825db92
sergiokhark <sergio.khark@gmail.com>
2022-07-01 15:32:09 +0300

    154.3_DOM

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let elem1 = document.querySelector('#elem1');
-let elem2 = document.querySelector('#elem2');
 let button1 = document.querySelector('#button1');
 let button2 = document.querySelector('#button2');
+
 button1.addEventListener('click', function () {
-    alert(elem1.innerHTML);
+    elem1.innerHTML = 'Привет';
 });
+
 button2.addEventListener('click', function () {
-    alert(elem2.innerHTML);
+    elem1.innerHTML = 'Пока';
 });
