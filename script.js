107b21e3b023f1097e1ae8ad3869dde2b7930d9e
sergiokhark <sergio.khark@gmail.com>
2022-07-01 08:22:35 +0300

    145.1_Date

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 let now = new Date();
-let date = new Date(now.getFullYear(), now.getMonth(), 15);
-if (date > now) {
-    console.log('Половина месяца еще не прошла');
-} else {
-    console.log('Половина месяца уже прошла');
+let date = new Date(now.getFullYear(), 2, 8);
+let dateNewYear = new Date(now.getFullYear() + 1, 2, 8);
+if (date - now > 0) {
+    console.log('До праздника 8 марта осталось дней: ', (date - now) / (1000 * 60 * 60 * 24));
+}
+if (date == now) {
+    console.log('Праздник 8 марта сегодня');
+}
+if (date - now < 0) {
+    console.log('Праздник 8 марта в этом году уже был. До следующего осталось дней: ',
+        (dateNewYear - now) / (1000 * 60 * 60 * 24));
 }
