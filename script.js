38c6d78514b81416c46c9e81847a5c8cf179867b
sergiokhark <sergio.khark@gmail.com>
2022-07-01 21:05:40 +0300

    156.2_DOM

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-let picture = document.querySelector('#picture');
-let button1 = document.querySelector('#button1');
-let button2 = document.querySelector('#button2');
-
-button1.addEventListener('click', function () {
-    picture.src = 'img/pic1.jpg';
+let elem = document.querySelector('#elem');
+let button = document.querySelector('#button');
+button.addEventListener('click', function () {
+    elem.value = 'New message';
 });
-button2.addEventListener('click', function () {
-    picture.src = 'img/pic2.jpg';
-});
-
