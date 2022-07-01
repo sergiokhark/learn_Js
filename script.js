77e09fcec87b1ce412ba542a7c0d782c7daf9f2d
sergiokhark <sergio.khark@gmail.com>
2022-07-01 21:26:43 +0300

    156.7_DOM

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let elem1 = document.querySelector('#elem1');
 let elem2 = document.querySelector('#elem2');
+let elem3 = document.querySelector('#elem3');
+let elem4 = document.querySelector('#elem4');
+let elem5 = document.querySelector('#elem5');
 let button = document.querySelector('#button');
+let elem = document.querySelector('#elem');
 
 button.addEventListener('click', function () {
-    let box = elem1.value;
-    elem1.value = elem2.value;
-    elem2.value = box;
-
+    elem.innerHTML = (Number(elem1.value)+ Number(elem2.value) + Number(elem3.value) + Number(elem4.value) + Number(elem5.value)) / 5;
 });
