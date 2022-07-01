1c90bc4b9d9577ae5eacfd68ce36ffff64cb6373
sergiokhark <sergio.khark@gmail.com>
2022-07-01 08:05:45 +0300

    144.1_Date

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-let date1 = '08.19';
-
-if (date1 >= '03.21' && date1 <= '04.20') { console.log('Овен') };
-if (date1 >= '04.21' && date1 <= '05.20') { console.log('Телец') };
-if (date1 >= '05.21' && date1 <= '06.20') { console.log('Близнецы') };
-if (date1 >= '06.21' && date1 <= '07.22') { console.log('Рак') };
-if (date1 >= '07.23' && date1 <= '08.22') { console.log('Лев') };
-if (date1 >= '08.23' && date1 <= '09.22') { console.log('Дева') };
-if (date1 >= '09.23' && date1 <= '10.22') { console.log('Весы') };
-if (date1 >= '10.23' && date1 <= '11.22') { console.log('Скорпион') };
-if (date1 >= '11.23' && date1 <= '12.21') { console.log('Стрелец') };
-if (date1 >= '12.22' && date1 <= '12.31') { console.log('Козерог') };
-if (date1 >= '01.01' && date1 <= '01.19') { console.log('Козерог') };
-if (date1 >= '01.20' && date1 <= '02.19') { console.log('Водолей') };
-if (date1 >= '02.20' && date1 <= '03.20') { console.log('Рыбы') };
+let now = new Date();
+let date = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 12);
+if (date > now) {
+    console.log('Полдня еще не было');
+} else {
+    console.log('Полдень уже был');
+}
