ad8247d056d2e1f2bcbb32d711df0a558b71d9bc
sergiokhark <sergio.khark@gmail.com>
2022-07-01 21:13:58 +0300

    156.4_DOM

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ let elem2 = document.querySelector('#elem2');
 let button = document.querySelector('#button');
 
 button.addEventListener('click', function () {
-    elem2.innerHTML = elem1.value;
+    elem2.value = elem1.value ** 2;
 });
