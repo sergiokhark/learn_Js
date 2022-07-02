e1f57b0b5044c1914641801b8080e4924d509d6f
sergiokhark <sergio.khark@gmail.com>
2022-07-02 13:50:02 +0300

    160.4

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-let elem1 = document.querySelector('.elem1')
-let elem2 = document.querySelector('.elem2')
-let elem3 = document.querySelector('.elem3')
-let elem4 = document.querySelector('.elem4')
-let elem5 = document.querySelector('.elem5')
+let elem1 = document.querySelector('#elem1')
+let elem2 = document.querySelector('#elem2')
+let elem3 = document.querySelector('#elem3')
 
-elem1.addEventListener('click', func)
-elem2.addEventListener('click', func)
-elem3.addEventListener('click', func)
-elem4.addEventListener('click', func)
-elem5.addEventListener('click', func)
+elem1.addEventListener('blur', func)
+elem2.addEventListener('blur', func)
+elem3.addEventListener('blur', func)
 
 function func() {
-    this.innerHTML = this.innerHTML + '!'
+    this.value = this.value ** 2
 }
