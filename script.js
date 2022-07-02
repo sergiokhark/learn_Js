ec6e779e9cdd550e0d925a60d6e7b866c7c2671e
sergiokhark <sergio.khark@gmail.com>
2022-07-02 08:53:15 +0300

    158.2_DOM

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let elem = document.querySelector('#elem');
 let button = document.querySelector('#button');
 button.addEventListener('click', function () {
+    
+    elem.className = 'new_class';
     alert(elem.className);
 })
