027428d561a9c63194217b0f0a043abb850703ae
sergiokhark <sergio.khark@gmail.com>
2022-07-02 08:35:51 +0300

    157.2_DOM

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 let elem = document.querySelector('#elem');
-elem.addEventListener('focus', function () {
-    elem.value = 1;
-});
 elem.addEventListener('blur', function () {
-    elem.value = 2;
-});
+    alert(elem.value ** 2);
+})
