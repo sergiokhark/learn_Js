0b461d267dde690f5f145a993800eb1dc616b6f9
sergiokhark <sergio.khark@gmail.com>
2022-07-02 16:06:26 +0300

    162.1_DOM

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-let elems = document.querySelectorAll('#elem')
-let elem1 = document.querySelector('.elem1')
-let button = document.querySelector('#button')
-
-button.addEventListener('click', func)
-
 function func() {
-    let sum = 0
-    for (let elem of elems) {
-        sum += Number(elem.value)
-    }
-    elem1.innerHTML = sum
+	this.value = Number(this.value) + 1;
+}
+
+let elems = document.querySelectorAll('#elem')
+for (let elem of elems) {
+    elem.addEventListener('blur', func)
 }
