5f7a919878a612fa225e7ec791f9e8462418176c
sergiokhark <sergio.khark@gmail.com>
2022-07-03 18:43:53 +0300

    175.7_Pract

--- script.js
+++ script.js
@@ -3,12 +3,8 @@ let elem = document.querySelector('#elem')
 elem.addEventListener('blur', func)
 
 function func() {
-    let fio = elem.value.split(' ')
-    let arr = []
-    for (let i = 0; i < fio.length; i++) {
-        arr.push(fio[i][0].toUpperCase() + fio[i].slice(1))
-    }
-    elem.value = arr.join(' ')
+    let arr = elem.value.split(' ')
+    console.log('Количество введенных слов: ', arr.length)
 
 }
     
