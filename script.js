acdc756f5691ae1d13a4351c200de8f7775fd999
sergiokhark <sergio.khark@gmail.com>
2022-07-03 19:06:59 +0300

    175.10_Pract

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let elem = document.querySelector('#elem')
-let button = document.querySelector('#button')
 
-button.addEventListener('click', func)
+elem.addEventListener('blur', func)
 
 function func() {
-    if (elem.value.split('').reverse().join('') == elem.value) {
-        alert('Читается одинаково')
-    } else {
-        alert('Читается не одинаково')
+    let digits = elem.value.split('')
+    for (let elem of digits) {
+        if (elem == '3') {
+            alert('Число содержит 3')
+            break
+        } 
     }
-    
 }
     
