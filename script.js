dc052f4bd0538119f858ab1203961d1911e87b31
sergiokhark <sergio.khark@gmail.com>
2022-07-03 17:07:28 +0300

    174.5_DOM

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let elem = document.querySelector('#elem')
 elem.addEventListener('blur', function () {
     
-    if (elem.value.length != elem.dataset.length) {
+    if (elem.value.length < elem.dataset.min || elem.value.length > elem.dataset.max ) {
         alert('Error')
     }
 })
