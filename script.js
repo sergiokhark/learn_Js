7beedd76ada6cbf1f4839aa73710876a3ab4bb40
sergiokhark <sergio.khark@gmail.com>
2022-07-03 17:21:30 +0300

    174.7

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-let elem = document.getElementById('elem')
-elem.addEventListener('click', function () {
-    
-    let result = elem.dataset.productPrice * elem.dataset.productAmount
-    elem.innerHTML += result
-
-    })
+let elems = document.querySelectorAll('p')
+for (let i = 0; i < elems.length; i++) {
+    elems[i].dataset.num = i + 1;
+    console.log(elems[i].dataset.num)
+}
