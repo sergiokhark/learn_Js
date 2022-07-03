71bd7221b9ff6b2ddcc37d7e9b0d3a3b917243ea
sergiokhark <sergio.khark@gmail.com>
2022-07-03 18:49:27 +0300

    175.8_Pract

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ let elem = document.querySelector('#elem')
 elem.addEventListener('blur', func)
 
 function func() {
-    let arr = elem.value.split(' ')
-    console.log('Количество введенных слов: ', arr.length)
-
+    let arr = elem.value.split('.')
+    elem.value = arr.reverse().join('-')
 }
     
