05d2ab01d8a967c919ff1af1be689f1e4ae2e835
sergiokhark <sergio.khark@gmail.com>
2022-07-03 17:55:11 +0300

    175.1_Pract

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let elems = document.querySelectorAll('p')
-for (let i = 0; i < elems.length; i++) {
-    elems[i].dataset.num = i + 1;
-    console.log(elems[i].dataset.num)
-}
+let elem1 = document.querySelector('#elem1')
+let elem2 = document.querySelector('.new')
+
+elem1.addEventListener('blur', function () {
+    elem2.innerHTML += elem1.value
+})
