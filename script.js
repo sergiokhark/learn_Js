5694d2b89d85b04a0c11f7553bb9c3c0cb38d5a1
sergiokhark <sergio.khark@gmail.com>
2022-07-03 09:56:15 +0300

    169.3_DOM

--- script.js
+++ script.js
@@ -4,8 +4,5 @@ let button = document.querySelector('#button')
 button.addEventListener('click', func)
 
 function func() {
-    elem.style.fontSize = '20px'
-    elem.style.borderTop = '2px solid #0000FF'
-    elem.style.background = 'yellow'
-    
+    elem.style.cssFloat ='left'
 }
