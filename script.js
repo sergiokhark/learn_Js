d046669ad20775036e7f96c3ece2a1144b2a986e
sergiokhark <sergio.khark@gmail.com>
2022-07-03 08:30:01 +0300

    167.2_DOM

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 let elem = document.querySelector('#elem')
 let button = document.querySelector('#button')
-button.addEventListener('click', function () {
-    if (elem.getAttribute('value') === elem.value) {
-        console.log('Пользователь не изменил данные')
-    } else {
-        console.log('Пользователь изменил значение')
-    }
-})
+button.addEventListener('click', func)
+
+function func() {
+    elem.value = elem.getAttribute('value')
+
+}
