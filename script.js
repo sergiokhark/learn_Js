e87e463d8115b348ace587be6f5440f9bb01736e
sergiokhark <sergio.khark@gmail.com>
2022-07-03 10:16:24 +0300

    170.2_DOM

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-let elem = document.querySelector('.elem')
-let button = document.querySelector('#button')
+let elem = document.querySelector('p')
+let button1 = document.querySelector('#button1')
+let button2 = document.querySelector('#button2')
+let button3 = document.querySelector('#button3')
 
-button.addEventListener('click', func)
+button1.addEventListener('click', function () {
+    elem.classList.add('decorated')
+})
 
-function func() {
-    elem.style.cssFloat ='left'
-}
+button2.addEventListener('click', function () {
+    elem.classList.add('bolded')
+})
+
+button3.addEventListener('click', function () {
+    elem.classList.add('colored')
+})
+
