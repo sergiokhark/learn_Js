ff2d1b3a6da905eb31367284d3162fbebbf64675
sergiokhark <sergio.khark@gmail.com>
2022-07-04 11:07:15 +0300

    175.23_Pract

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-let elem = document.querySelector('#elem')
-let button = document.querySelector('#button')
+let link = document.querySelector('a')
+let elem = document.querySelector('#text')
 
-button.addEventListener('click', func)
+link.addEventListener('click', func)
 
 function func() {
-    let symbols = elem.value.split('')
-    let resArr = []
-    while (symbols.length > 0) {
-        let randomInd = getRandomInd(0, symbols.length - 1)
-        let elem = symbols.splice(randomInd, 1)[0];
-        resArr.push(elem)
-    }
-    elem.value = resArr.join('')
-}
-
-function getRandomInd(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min
-}
+	
+	if (elem.checked) {
+		elem.checked = false;
+ 	} else {
+		elem.checked = true;
+ 	}
+  }
