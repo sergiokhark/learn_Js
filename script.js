57d146e343b418f4373d2cbbd706316d195d82a6
sergiokhark <sergio.khark@gmail.com>
2022-07-04 12:17:23 +0300

    177.2_Forms

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let elem = document.querySelector('#elem')
-let str = document.querySelector('p')
+let button1 = document.querySelector('#button1')
+let button2 = document.querySelector('#button2')
 
-elem.addEventListener('blur', function () {
-    str.innerHTML = elem.value
+button1.addEventListener('click', function () {
+    elem.disabled = true
 
 })
 
+button2.addEventListener('click', function () {
+    elem.disabled = false
 
+})
