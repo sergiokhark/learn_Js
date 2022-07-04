d81bda61cc5bf048d4c9904abd70f32c431bbbf4
sergiokhark <sergio.khark@gmail.com>
2022-07-04 12:48:50 +0300

    179.2_Forms

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 let elem = document.querySelector('#elem')
 let button = document.querySelector('#button')
-let str = document.querySelector('p')
 
 button.addEventListener('click', function () {
     if (elem.checked) {
-        str.innerHTML = 'Привет'
+        elem.checked = false
     } else {
-        str.innerHTML = 'Пока'
+        elem.checked = true
     }
 })
