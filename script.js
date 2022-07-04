bd9f60fd21887577885576669064d6377e73994e
sergiokhark <sergio.khark@gmail.com>
2022-07-04 15:22:32 +0300

    182.2_Forms

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let elem = document.querySelector('#elem')
+let str = document.querySelector('p')
 
 elem.addEventListener('input', function () {
-    if (this.value.length == 5) {
-        alert('Введено 5 символов')
+    if (this.value.length <= 5) {
+        str.innerHTML = 'Разрешено ввести еще символов:' + (5 - this.value.length)
+    } else {
+        str.innerHTML = 'Ввод превышен на ' + (this.value.length - 5)
     }
+
+
 })
