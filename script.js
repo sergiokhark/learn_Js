4e0f38dad367d1bedff236d1fde49875066459aa
sergiokhark <sergio.khark@gmail.com>
2022-07-04 09:16:54 +0300

    175.16_Pract

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-let elem = document.querySelector('#elem')
-let button1 = document.querySelector('#button1')
-let button2 = document.querySelector('#button2')
+let elems = document.querySelectorAll('p')
+let inpElem = document.querySelector('#elem')
 
-button1.addEventListener('click', function () {
-    elem.value = Number(elem.value) + 1
-})
+for (let elem of elems) {
+    
+    elem.addEventListener('click', function () {
+        inpElem.value++
 
-button2.addEventListener('click', function () {
-    if (elem.value != 0) {
-        elem.value = Number(elem.value) - 1
-    }  
-})
+    })
+}
