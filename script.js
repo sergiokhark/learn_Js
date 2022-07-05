6eaf7931192a141297090731877bbf8dbccdee92
sergiokhark <sergio.khark@gmail.com>
2022-07-05 16:03:31 +0300

    193.1_Events

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 let elem = document.querySelector('#elem')
+let str = document.querySelector('p')
 
-elem.addEventListener('click', function (event) {
-    if (event.target.tagName == 'LI') {
-        event.target.innerHTML += '!'
+elem.addEventListener('keydown', function (event) {
+    if (event.code == 'Enter') {
+        str.innerHTML = elem.value
+        elem.value = ''
     }
-
-    if (event.target.tagName == 'UL') {
-        event.target.innerHTML += '<li>text</li>'
-    }
-    
 })
