4f07e24258fefafef1683c7967cf3c507e249247
sergiokhark <sergio.khark@gmail.com>
2022-07-05 16:23:18 +0300

    194.2_Event

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let elem = document.querySelector('#elem');
 
 elem.addEventListener('click', function (event) {
-    if (event.altKey) {
-        elem.classList.add('colred')
+    if (event.target.tagName == 'LI' && event.ctrlKey) {
+        event.target.innerHTML += 1
+    }
+
+    if (event.target.tagName == 'LI' && event.shiftKey) {
+        event.target.innerHTML += 2
     }
 
 })
