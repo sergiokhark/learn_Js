033411b79a9bdfa2da934dbdd41b0be02a8748bd
sergiokhark <sergio.khark@gmail.com>
2022-07-06 16:21:23 +0300

    211.7_Timer

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-let button1 = document.querySelector('#button1')
-let button2 = document.querySelector('#button2')
 let str = document.querySelector('#str')
-let timeId
 
-button1.addEventListener('click', function () {
-	timeId = setInterval(function () {
-		str.innerHTML = Number(str.innerHTML) + 1
-	}, 300)
-	
-})
-
-button2.addEventListener('click', function () {
-	clearInterval(timeId)
-})
+str.classList.add('colred')
+setInterval(function () {
+	str.classList.toggle('colred')
+	str.classList.add('colgreen')
+}, 1000)
