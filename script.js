7897519c38ff316ff2f01e2ad6a814420ec87eba
sergiokhark <sergio.khark@gmail.com>
2022-07-06 13:37:15 +0300

    208.2_Timer

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-let start = document.querySelector('#start')
-let stop  = document.querySelector('#stop')
-let timerId
+let elem = document.querySelector('#elem')
 
-start.addEventListener('click', func)
-	
-function func() {
-	timerId = setInterval(function() {
-		let date = new Date
-		console.log(date.getMinutes() + ' ' + date.getSeconds())
-	}, 1000)
-	this.removeEventListener('click', func)
-}
-	
-stop.addEventListener('click', function() {
-	clearInterval(timerId)
-	start.addEventListener('click', func)
-});
+setInterval(function () {
+	elem.value = Number(elem.value) - 1
+}, 500)
