ec63123be4abe4d7b8e11106df9486828b8fb51d
sergiokhark <sergio.khark@gmail.com>
2022-07-06 15:32:49 +0300

    211.4_Timer

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-let str = document.querySelector('#elem')
+let elem = document.querySelector('#elem')
+let str = document.querySelector('#str')
 
-setInterval(function () {
-	str.value = Number(str.value) ** 2
-},500)
+elem.addEventListener('blur', func)
+
+function func() {
+	str.innerHTML = this.value
+	let timeId = setInterval(function () {
+		str.innerHTML = Number(str.innerHTML) - 1
+		if (str.innerHTML <= 0) {
+			clearInterval(timeId)
+		}
+	}, 300)
+}
