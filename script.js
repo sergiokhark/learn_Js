2e06afac4dafe5ef0ded159b9f999dee757bcdc9
sergiokhark <sergio.khark@gmail.com>
2022-07-06 16:35:05 +0300

    211.8_Timer

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ let str = document.querySelector('#str')
 
 setInterval(function () {
 	let now = new Date
-	str.innerHTML = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds()
+	str.innerHTML = getZero(now.getHours()) + ':' + getZero(now.getMinutes()) + ':' + getZero(now.getSeconds())
 
 }, 1000)
+
+function getZero(num) {
+	if (num <= 9) {
+		return '0' + num
+	}
+	else {
+		return num
+	}
+}
