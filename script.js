387fb9803bbab7137de220105ac971e4c3e3d322
sergiokhark <sergio.khark@gmail.com>
2022-07-07 19:19:15 +0300

    222.3_Elements

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let employees = [
 	{name: 'employee1', age: 30, salary: 400},
 	{name: 'employee2', age: 31, salary: 500},
-	{name: 'employee3', age: 32, salary: 600},
+	{name: 'employee3', age: 32, salary: 400},
 ]
 let table = document.querySelector('#table')
 
@@ -15,14 +15,14 @@ for (let elem of employees) {
 	let td2 = document.createElement('td')
 	td2.innerHTML = elem.age
 	tr.appendChild(td2)
-	td2.addEventListener('click', function () {
-		td2.innerHTML++
-	})
 	
-
 	let td3 = document.createElement('td')
 	td3.innerHTML = elem.salary
 	tr.appendChild(td3)
+
+	td3.addEventListener('click', function() {
+		td3.innerHTML *= 1.1
+	})
 
 	table.appendChild(tr)
 
