82d599af7aca4f1c841ceb20aa6fee8766558579
sergiokhark <sergio.khark@gmail.com>
2022-07-07 15:03:49 +0300

    213.2_Elements

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 let elem = document.querySelector('#elem')
-let li = document.createElement('li')
-li.innerHTML = 'пункт'
-elem.appendChild(li)
+let button = document.querySelector('#button')
+
+button.addEventListener('click', function () {
+	let li = document.createElement('li')
+	li.innerHTML = 'пункт'
+	
+	elem.appendChild(li)
+	
+})
