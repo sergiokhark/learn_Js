fe546f8a90458a7b6e36cf75c4e34c465a434132
sergiokhark <sergio.khark@gmail.com>
2022-07-07 20:29:36 +0300

    223.1_Elements

--- script.js
+++ script.js
@@ -1,29 +1,12 @@
-let employees = [
-	{name: 'employee1', age: 30, salary: 400},
-	{name: 'employee2', age: 31, salary: 500},
-	{name: 'employee3', age: 32, salary: 400},
-]
 let table = document.querySelector('#table')
+let button = document.querySelector('#button')
 
-for (let elem of employees) {
+button.addEventListener('click', function () {
 	let tr = document.createElement('tr')
-	
-	let td1 = document.createElement('td')
-	td1.innerHTML = elem.name
-	tr.appendChild(td1)
+	let td = document.createElement('td')
 
-	let td2 = document.createElement('td')
-	td2.innerHTML = elem.age
-	tr.appendChild(td2)
-	
-	let td3 = document.createElement('td')
-	td3.innerHTML = elem.salary
-	tr.appendChild(td3)
-
-	td3.addEventListener('click', function() {
-		td3.innerHTML *= 1.1
-	})
-
+	tr.appendChild(td)
 	table.appendChild(tr)
 
-}
+})
+
