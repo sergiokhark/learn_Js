0fb823e51d1bbb0201d718cd1fe18079edbbd7d6
sergiokhark <sergio.khark@gmail.com>
2022-07-07 18:16:45 +0300

    219.3_Elements

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let table = document.querySelector('#table')
 
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 10; i++) {
 	let tr = document.createElement('tr')
 	
 	for (let j = 0; j < 5; j++) {
 		let td = document.createElement('td')
+		td.innerHTML = 'x'
 		tr.appendChild(td)
 	}
 
