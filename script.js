96e759fdcca86a2133998ce280c99be0d7550eed
sergiokhark <sergio.khark@gmail.com>
2022-07-07 18:45:12 +0300

    220.1_Elements

--- script.js
+++ script.js
@@ -1,30 +1,15 @@
 let table = document.querySelector('#table')
-let elem1 = document.querySelector('#elem1')
-let elem2 = document.querySelector('#elem2')
-let button = document.querySelector('#button')
+let k = 1
 
-
-
-button.addEventListener('click', function () {
-	let lines = Number(elem1.value)
-	let columns = Number(elem2.value)
-	
-	for (let i = 0; i < lines; i++) {
+for (let i = 0; i < 5; i++) {
 	let tr = document.createElement('tr')
-	
-	for (let j = 0; j < columns; j++) {
+
+	for (let j = 0; j < 5; j++) {
 		let td = document.createElement('td')
-		td.innerHTML = 'x'
+		td.innerHTML = k
+		k++
 		tr.appendChild(td)
 	}
 
 	table.appendChild(tr)
 }
-
-
-})
-	
-
-
-
-
