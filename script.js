7f39a02e4fb3717ee479370698647c85f9f731f3
sergiokhark <sergio.khark@gmail.com>
2022-07-07 15:08:22 +0300

    214.1_Elements

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let button = document.querySelector('#button')
 button.addEventListener('click', function () {
 	let li = document.createElement('li')
 	li.innerHTML = 'пункт'
+	li.addEventListener('click', function () {
+		li.innerHTML += '!'
+	})
 	
 	elem.appendChild(li)
 	
