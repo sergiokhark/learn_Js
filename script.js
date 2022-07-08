ca904da1471a2e485596eee165fca1a9d5128072
sergiokhark <sergio.khark@gmail.com>
2022-07-08 13:00:29 +0300

    231.1_Elements

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 let parent = document.querySelector('#parent')
-let lis = parent.querySelectorAll('li')
+let button = document.querySelector('#button')
+let lis0 = parent.querySelectorAll('li')
 
-for (let li of lis) {
+for (let li of lis0) {
 	li.addEventListener('click', function () {
-		parent.removeChild(this)
+		this.remove()
 	})
 }
+
+button.addEventListener('click', function () {
+	let li = document.createElement('li')
+	li.innerHTML = 'new line'
+	li.addEventListener('click', function () {
+		this.remove()
+	})
+	parent.appendChild(li)
+	
+})
