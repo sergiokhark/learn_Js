02839f7bc7bda9ce0d45c161a1820f8bfc222598
sergiokhark <sergio.khark@gmail.com>
2022-07-08 17:44:19 +0300

    234.3_Elements

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ let elem = document.querySelector('#elem')
 
 let li = document.createElement('li')
 li.innerHTML = 'new'
+li.addEventListener('click', function () {
+	li.innerHTML += '!'
+})
 
 parent.insertBefore(li, elem)
