ec805abd35b086fbb48799f92e6100d0f661061e
sergiokhark <sergio.khark@gmail.com>
2022-07-08 11:02:39 +0300

    229.1_Elements

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
-let tds = document.querySelectorAll('#table td')
+let parent = document.querySelector('#parent')
+let button = document.querySelector('#button')
 
-let i = 1
-
-for (let td of tds) {
-	td.addEventListener('click', function func() {
-		this.innerHTML = i
-
-		if (i == 1) {
-			i = 2
-		} else {
-			if (i == 2) {
-				i = 3
-			} else {
-				i = 1
-			}
-		}
-		this.removeEventListener('click', func)
-	})
-}
+button.addEventListener('click', function () {
+	parent.removeChild(elem)
+})
