0d6a4b82ae3490cdd1f5f35807b581a3f96bdc8d
sergiokhark <sergio.khark@gmail.com>
2022-07-08 09:47:09 +0300

    225.1_Elements

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-let button = document.querySelector('#button')
 let table = document.querySelector('#table')
 let tds = document.querySelectorAll('#table td')
 
-
-button.addEventListener('click', function () {
-	for (let td of tds) {
-		td.innerHTML *= 2
-	}
-})
+let i = tds.length
+for (let td of tds) {
+	td.innerHTML = i--
+}
