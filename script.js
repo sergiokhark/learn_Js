df7e0512a14ccc23d066af7e6522c321e58047ce
sergiokhark <sergio.khark@gmail.com>
2022-07-08 10:29:46 +0300

    228.2_Elements

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 let tds = document.querySelectorAll('#table td')
 
 let i = 1
+
 for (let td of tds) {
 	td.addEventListener('click', function func() {
 		this.innerHTML = i
-		i++
+
+		if (i == 1) {
+			i = 2
+		} else {
+			if (i == 2) {
+				i = 3
+			} else {
+				i = 1
+			}
+		}
 		this.removeEventListener('click', func)
 	})
 }
