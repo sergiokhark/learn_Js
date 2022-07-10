f697799ab1c3d0fac801ad6cdedefead5a6508a7
sergiokhark <sergio.khark@gmail.com>
2022-07-10 14:12:48 +0300

    247.3_Elements

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ let tds = document.querySelectorAll('td')
 
 for (let td of tds) {
 	td.addEventListener('click', function () {
-		
-		if ((!td.previousElementSibling.classList.contains('active') &&
-			!td.nextElementSibling.classList.contains('active')) ||
-			td.previousElementSibling === null || td.nextElementSibling === null) {
-			
+		let prev = td.previousElementSibling
+		let next = td.nextElementSibling
+		if (prev === null && !next.classList.contains('active')) {
 			td.classList.add('active')
 		}
-	})
-}
+		if (next === null && !prev.classList.contains('active')) {
+			td.classList.add('active')
+		}
+		if (next !== null && prev !== null && !next.classList.contains('active') && !prev.classList.contains('active')) {
+			td.classList.add('active')
+		}
+	})		
+}	
