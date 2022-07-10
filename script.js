bc383bb126fa1b120df2eedd3bd417e5f14d0de0
sergiokhark <sergio.khark@gmail.com>
2022-07-10 10:10:42 +0300

    241.2_Elements

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 let elems = document.querySelectorAll('#parent p')
-let spans = document.querySelectorAll('#parent span')
+for (let elem of elems) {
+	elem.innerHTML = '<span>' + elem.innerHTML + '</span>'
+}
+
+let elems2 = document.querySelectorAll('#parent span')
+for (let elem2 of elems2) {
+	elem2.addEventListener('click', function func() {
+		let input = document.createElement('input')
+		input.value = elem2.innerHTML
+		elem2.innerHTML = ''
+		elem2.appendChild(input)
+		elem2.removeEventListener('click', func)
+		input.addEventListener('blur', function () {
+			elem2.innerHTML = input.value
+			elem2.addEventListener('click', func)
+		})
+	})
+}
 
 for (let elem of elems) {
 	let link = document.createElement('a')
-	link.innerHTML = 'Delete'
+	link.innerHTML = 'delete'
 	link.href = '#'
 	elem.appendChild(link)
 	link.addEventListener('click', function () {
@@ -11,17 +28,3 @@ for (let elem of elems) {
 	})
 }
 
-for (let span of spans) {
-	span.addEventListener('click', function func() {
-		let input = document.createElement('input')
-		input.value = span.innerHTML
-		span.innerHTML = ''
-		span.appendChild(input)
-		span.removeEventListener('click', func)
-
-		input.addEventListener('blur', function () {
-			span.innerHTML = input.value
-			span.addEventListener('click', func)
-		})
-	})
-}
