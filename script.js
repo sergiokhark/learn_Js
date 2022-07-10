c50ef03e716668b118bb973994052a008ecd2105
sergiokhark <sergio.khark@gmail.com>
2022-07-10 11:12:15 +0300

    244.3_Elements

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let buttons = document.querySelectorAll('button')
-let elems = document.querySelectorAll('p')
 
-for (let i = 0; i < buttons.length; i++) {
-	buttons[i].addEventListener('click', function () {
-		elems[i].classList.toggle('hidden')
+for (let button of buttons) {
+	button.addEventListener('click', function () {
+		button.previousElementSibling.classList.toggle('hidden')
 	})
 }
