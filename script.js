c5e980356d7f79a1a9cb599689e87d13365d27d2
sergiokhark <sergio.khark@gmail.com>
2022-07-10 10:33:30 +0300

    242.1_Elements

--- script.js
+++ script.js
@@ -1,30 +1,18 @@
-let elems = document.querySelectorAll('#parent p')
+let elems = document.querySelectorAll('p')
+
 for (let elem of elems) {
 	elem.innerHTML = '<span>' + elem.innerHTML + '</span>'
 }
 
-let elems2 = document.querySelectorAll('#parent span')
+let elems2 = document.querySelectorAll('span')
+
 for (let elem2 of elems2) {
-	elem2.addEventListener('click', function func() {
-		let input = document.createElement('input')
-		input.value = elem2.innerHTML
-		elem2.innerHTML = ''
-		elem2.appendChild(input)
-		elem2.removeEventListener('click', func)
-		input.addEventListener('blur', function () {
-			elem2.innerHTML = input.value
-			elem2.addEventListener('click', func)
-		})
-	})
-}
-
-for (let elem of elems) {
 	let link = document.createElement('a')
-	link.innerHTML = 'delete'
+	link.innerHTML = 'push'
 	link.href = '#'
-	elem.appendChild(link)
+	elem2.parentElement.appendChild(link)
 	link.addEventListener('click', function () {
-		elem.parentElement.removeChild(elem)
+		elem2.classList.add('decoration')
 	})
 }
-
+	
