71241a3deca8b62976732736b5eec968bb2e7cbf
sergiokhark <sergio.khark@gmail.com>
2022-07-10 09:18:00 +0300

    241.1_Elements

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-let tds = document.querySelectorAll('#table td')
+let elems = document.querySelectorAll('#parent p')
+let spans = document.querySelectorAll('#parent span')
 
-for (let td of tds) {
-	td.addEventListener('click', function func() {
+for (let elem of elems) {
+	let link = document.createElement('a')
+	link.innerHTML = 'Delete'
+	link.href = '#'
+	elem.appendChild(link)
+	link.addEventListener('click', function () {
+		elem.parentElement.removeChild(elem)
+	})
+}
+
+for (let span of spans) {
+	span.addEventListener('click', function func() {
 		let input = document.createElement('input')
-		input.value = td.innerHTML
-		td.innerHTML = ''
-		td.appendChild(input)
-		td.removeEventListener('click', func)
+		input.value = span.innerHTML
+		span.innerHTML = ''
+		span.appendChild(input)
+		span.removeEventListener('click', func)
 
 		input.addEventListener('blur', function () {
-			td.innerHTML = input.value
-			td.addEventListener('click', func)
+			span.innerHTML = input.value
+			span.addEventListener('click', func)
 		})
 	})
-	
 }
