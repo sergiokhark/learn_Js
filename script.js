c4b3973c9818c0e883ad1b75dbb811aad863cf4a
sergiokhark <sergio.khark@gmail.com>
2022-07-11 19:09:49 +0300

    248.6_Elements

--- script.js
+++ script.js
@@ -1,58 +1,25 @@
-let arr = [18, 22, 12, 55, 556]
-let parent = document.querySelector('#parent')
-let elem = document.querySelector('#elem')
+let employees = [
+	{name: 'employee1', age: 30, salary: 400},
+	{name: 'employee2', age: 31, salary: 500},
+	{name: 'employee3', age: 32, salary: 600},
+]
 
-for (let elem of arr) {										
-	let li = document.createElement('li')
-	li.innerHTML = '<span>' + elem + '</span>'
-	let link = addDeleteLink(li)
-	li.appendChild(link)
-	li.firstElementChild.addEventListener('click', getEdit)
-	parent.appendChild(li)
-	let link2 = addStyleLink(li)
-	li.appendChild(link2)
-}
+let table = document.querySelector('#table')
 
-elem.addEventListener('blur', function () {                 
-	let li = document.createElement('li')
-	li.innerHTML = '<span>' + elem.value + '</span>'
-	let link = addDeleteLink(li)
-	li.appendChild(link)
-	li.firstElementChild.addEventListener('click', getEdit)
-	parent.appendChild(li)
-	let link2 = addStyleLink(li)
-	li.appendChild(link2)
-})
+for (let employee of employees) {
+	let tr = document.createElement('tr')
+	
+	let td1 = document.createElement('td')
+	td1.innerHTML = employee.name
+	tr.appendChild(td1)
+	
+	let td2 = document.createElement('td')
+	td2.innerHTML = employee.age
+	tr.appendChild(td2)
 
-function getEdit() {			
-	let inp = document.createElement('input')
-	inp.value = this.innerHTML
-	this.innerHTML = ''
-	this.appendChild(inp)
-	this.removeEventListener('click', getEdit)
-	let self = this
-	inp.addEventListener('blur', function () {
-		self.innerHTML = inp.value
-		self.addEventListener('click', getEdit)
-	})
-}
+	let td3 = document.createElement('td')
+	td3.innerHTML = employee.salary
+	tr.appendChild(td3)
 
-function addDeleteLink(li) {
-	let link = document.createElement('a')			
-	link.innerHTML = 'удалить'
-	link.href = '#'
-	link.addEventListener('click', function () {
-		parent.removeChild(li)
-	})
-	return link
-}
-
-function addStyleLink(li) {
-	let link = document.createElement('a')
-	link.innerHTML = 'перечеркнуть'
-	link.href = '#'
-	link.addEventListener('click', function () {
-		li.firstElementChild.classList.add('decorated')
-	})
-	return link
+	table.appendChild(tr)
 }
