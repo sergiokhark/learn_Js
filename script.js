d8fa5643a871939db909f368823e2084b5254393
sergiokhark <sergio.khark@gmail.com>
2022-07-11 20:00:10 +0300

    248.10_Elements

--- script.js
+++ script.js
@@ -4,83 +4,23 @@ let employees = [
 	{name: 'employee3', age: 32, salary: 600},
 ]
 
-let table = document.querySelector('#table')
+let parent = document.querySelector('#parent')
 
 for (let employee of employees) {
-	let tr = document.createElement('tr')
+	let ul = document.createElement('ul')
 	
-	let td1 = document.createElement('td')
-	td1.innerHTML = employee.name
-	tr.appendChild(td1)
-	
-	let td2 = document.createElement('td')
-	td2.innerHTML = employee.age
-	tr.appendChild(td2)
+	let li1 = document.createElement('li')
+	li1.innerHTML = employee.name
+	ul.appendChild(li1)
 
-	let td3 = document.createElement('td')
-	td3.innerHTML = employee.salary
-	tr.appendChild(td3)
+	let li2 = document.createElement('li')
+	li2.innerHTML = employee.age
+	ul.appendChild(li2)
 
-	table.appendChild(tr)
+	let li3 = document.createElement('li')
+	li3.innerHTML = employee.salary
+	ul.appendChild(li3)
+
+	parent.appendChild(ul)
 }
 
-let tds = document.querySelectorAll('td')
-
-for (let td of tds) {
-	td.addEventListener('click', edit)
-}
-
-function edit() {
-	let input = document.createElement('input')
-	input.value = this.innerHTML
-	this.innerHTML = ''
-	this.appendChild(input)
-	this.removeEventListener('click', edit)
-	let self = this
-	input.addEventListener('blur', function () {
-		self.innerHTML = input.value
-		self.addEventListener('click', edit)
-	})
-	
-}
-
-let trs = document.querySelectorAll('tr')
-
-for (let tr of trs) {
-	let td = document.createElement('td')
-	let link = document.createElement('a')
-	link.innerHTML = 'Delete'
-	link.href = '#'
-	link.addEventListener('click', function () {
-		table.removeChild(tr)
-	})
-
-	td.appendChild(link)
-	tr.appendChild(td)
-}
-
-let input1 = document.querySelector('#input1')
-let input2 = document.querySelector('#input2')
-let input3 = document.querySelector('#input3')
-let button = document.querySelector('#button')
-
-button.addEventListener('click', function () {
-	let tr = document.createElement('tr')
-	
-	let td1 = document.createElement('td')
-	td1.innerHTML = input1.value
-	td1.addEventListener('click', edit)
-	tr.appendChild(td1)
-
-	let td2 = document.createElement('td')
-	td2.innerHTML = input2.value
-	td2.addEventListener('click', edit)
-	tr.appendChild(td2)
-
-	let td3 = document.createElement('td')
-	td3.innerHTML = input3.value
-	td3.addEventListener('click', edit)
-	tr.appendChild(td3)
-
-	table.appendChild(tr)
-})
