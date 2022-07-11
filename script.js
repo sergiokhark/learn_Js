7d81d5f45c3acaaae509ea1440f0669c150361e3
sergiokhark <sergio.khark@gmail.com>
2022-07-11 10:28:07 +0300

    248.3_v3_Elements

--- script.js
+++ script.js
@@ -2,36 +2,28 @@ let arr = [18, 22, 12, 55, 556]
 let parent = document.querySelector('#parent')
 let elem = document.querySelector('#elem')
 
+function getEdit() {			
+	let inp = document.createElement('input')
+	inp.value = this.innerHTML
+	this.innerHTML = ''
+	this.appendChild(inp)
+	this.removeEventListener('click', getEdit)
+	inp.addEventListener('blur', function () {
+		li.innerHTML = inp.value
+		li.addEventListener('click', getEdit)
+	})
+}
+
 for (let elem of arr) {										//делаем из массива строки Li
 	let li = document.createElement('li')
 	li.innerHTML = elem
-	li.addEventListener('click', function func() {			
-		let inp1 = document.createElement('input')
-		inp1.value = li.innerHTML
-		li.innerHTML = ''
-		li.appendChild(inp1)
-		li.removeEventListener('click', func)
-		inp1.addEventListener('blur', function () {
-			li.innerHTML = inp1.value
-			li.addEventListener('click', func)
-		})
-	})
+	li.addEventListener('click', getEdit)
 	parent.appendChild(li)
 }
 
 elem.addEventListener('blur', function () {                 //создаем строки li из инпута
 	let li = document.createElement('li')
 	li.innerHTML = elem.value
-	li.addEventListener('click', function func() {
-		let inp = document.createElement('input')
-		inp.value = li.innerHTML
-		li.innerHTML = ''
-		li.appendChild(inp)
-		li.removeEventListener('click', func)
-		inp.addEventListener('blur', function () {
-			li.innerHTML = inp.value
-			li.addEventListener('click', func)
-		})
-	})
+	li.addEventListener('click', getEdit)
 	parent.appendChild(li)	
 })
