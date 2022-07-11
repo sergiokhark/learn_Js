652bdd6837251c7a7d5fb13e8628100ccadd614d
sergiokhark <sergio.khark@gmail.com>
2022-07-11 08:30:32 +0300

    248.3_v2_Elements

--- script.js
+++ script.js
@@ -5,23 +5,18 @@ let elem = document.querySelector('#elem')
 for (let elem of arr) {										//делаем из массива строки Li
 	let li = document.createElement('li')
 	li.innerHTML = elem
-	parent.appendChild(li)
-}
-
-let lis = parent.querySelectorAll('li')
-
-for (let li of lis) {
-	li.addEventListener('click', function func() {			// редактируем li из массива
-		let inp = document.createElement('input')
-		inp.value = li.innerHTML
+	li.addEventListener('click', function func() {			
+		let inp1 = document.createElement('input')
+		inp1.value = li.innerHTML
 		li.innerHTML = ''
-		li.appendChild(inp)
+		li.appendChild(inp1)
 		li.removeEventListener('click', func)
-		inp.addEventListener('blur', function () {
-			li.innerHTML = inp.value
+		inp1.addEventListener('blur', function () {
+			li.innerHTML = inp1.value
 			li.addEventListener('click', func)
 		})
 	})
+	parent.appendChild(li)
 }
 
 elem.addEventListener('blur', function () {                 //создаем строки li из инпута
