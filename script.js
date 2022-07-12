413bf3c81b5a36a2fc6f77eb62bc17d3d11c1a0f
sergiokhark <sergio.khark@gmail.com>
2022-07-12 15:59:05 +0300

    255.1_Func

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function forEach(selector, func) {
 	let elems = document.querySelectorAll(selector)
-	for (let elem of elems) {
-		func(elem)
+	
+	for (let i = 0; i < elems.length; i++) {
+		func(elems[i], i)
 	}
 }
 
-forEach('.elem', function (elem) {
-	elem.innerHTML += '!'
-})
+forEach('.elem', function(elem, index) {
+	elem.innerHTML =  index + elem.innerHTML
+});
