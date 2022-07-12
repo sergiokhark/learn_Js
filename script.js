e94a39f48de1faca47eee252b129b1d7f1b70f38
sergiokhark <sergio.khark@gmail.com>
2022-07-12 17:03:54 +0300

    257.3_Func

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+let elem = document.querySelector('#elem')
+let arr = [123, 345, 44, 18, 55, 900]
+
 function appendElem(elem, text) {
 	let li = document.createElement('li')
 	li.innerHTML = text
 	elem.appendChild(li)
 }
 
-
-let elem = document.querySelector('#elem')
-
-appendElem(elem, '!!!!')
+for (let text of arr) {
+	appendElem(elem, text)
+}
