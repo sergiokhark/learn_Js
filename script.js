9b0a6f514a9d1cc822b60ae0c7326e9f23b4dec2
sergiokhark <sergio.khark@gmail.com>
2022-07-12 13:35:46 +0300

    251.2_Search

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let tds = document.querySelectorAll('#table td:nth-child(3)')
+let tds = document.querySelectorAll('#table tr:nth-child(1) td:nth-child(3)')
 
 for (let td of tds) {
 	td.classList.add('colred')
