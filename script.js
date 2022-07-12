c7200616cb7c66a13c136c233f734e304e345c98
sergiokhark <sergio.khark@gmail.com>
2022-07-12 13:39:29 +0300

    251.3_Search

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let tds = document.querySelectorAll('#table tr:nth-child(1) td:nth-child(3)')
+let tds = document.querySelectorAll('#table tr:nth-child(even) td:nth-child(odd)')
 
 for (let td of tds) {
 	td.classList.add('colred')
