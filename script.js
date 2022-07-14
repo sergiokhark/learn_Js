5a5e5565ad173ccefeeae6aa78f9a79f44609c09
sergiokhark <sergio.khark@gmail.com>
2022-07-14 13:56:47 +0300

    6.2_Vue

--- script.js
+++ script.js
@@ -4,15 +4,8 @@ let app = new Vue({
 		text: 'cтрока',
 	},
 	methods: {
-		upperCaseFirst: function () {
-			this.text = this.text[0].toUpperCase() + this.text.slice(1)
-		},
-		upperCaseLast: function () {
-			this.text = this.text.slice(0, this.text.length - 1) + this.text[this.text.length - 1].toUpperCase()
-		},
-		upperCase: function () {
-			this.upperCaseFirst()
-			this.upperCaseLast()
+		sayHello: function (name) {
+			alert('Привет, ' + name + '!')
 		}
 	}
 });
