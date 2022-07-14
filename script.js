d9dd3c49defad0da860fe45a92418738a20b85da
sergiokhark <sergio.khark@gmail.com>
2022-07-14 17:06:04 +0300

    7.10_Vue

--- script.js
+++ script.js
@@ -2,11 +2,19 @@ let app = new Vue({
 	el: '#app',
 	data: {
 		text: 'value',
-		show: true,
+		show1: true,
+		show2: true,
+		show3: true,
 	},
 	methods: {
-		toggleElem: function () {
-			this.show = !this.show
+		toggleElem1: function () {
+			this.show1 = !this.show1
+		},
+		toggleElem2: function () {
+			this.show2 = !this.show2
+		},
+		toggleElem3: function () {
+			this.show3 = !this.show3
 		}
 		
 	},
