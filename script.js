1826468cc3ed8f3b937941eb130031d0ab55b05d
sergiokhark <sergio.khark@gmail.com>
2022-07-14 15:36:18 +0300

    7.1_Vue

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 let app = new Vue({
 	el: '#app',
 	data: {
-		arr: [1, 12, 18, 23, 45, 44, 55, 3, 4],
+		show: false,
 	},
-	methods: {
-		addElem: function (index) {
-			let elem = this.arr.splice(index, 1) + '!'
-			this.arr.splice(index, 0, elem)
-		}
-	}
+	
 });
 
