8f368dd3a04953aa49477da42304b6cd4e8b1b5e
sergiokhark <sergio.khark@gmail.com>
2022-07-14 11:44:52 +0300

    5.1_Vue

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 let app = new Vue({
 	el: '#app',
 	data: {
-		num1: 1,
-		num2: 2,
-		num3: 3,
+		text: '12345',
 	},
 	methods: {
-		sum: function () {
-			alert(this.num1 + this.num2 + this.num3)
+		changeMessage: function() {
+			this.text = 'abcde';
 		}
 	}
-})
+});
 
