e3db847956ae0fa8fcfcd84732745c311f6a8459
sergiokhark <sergio.khark@gmail.com>
2022-07-16 14:10:21 +0300

    14.8_Vue

--- script.js
+++ script.js
@@ -1,43 +1,12 @@
 let app = new Vue({
 	el: '#app',
 	data: {
-		name: '',
-		price: '',
-		quantity: '',
-		total: 0
+		users: [
+			{name: 'Коля', age: 30},
+			{name: 'Вася', age: 40},
+			{name: 'Петя', age: 50},
+		]
 	},
-	methods: {
-		addProduct: function (...rest) {
-			let table = document.querySelector('table')
-			let tr = document.createElement('tr')
-			for (let elem of rest) {
-				let td = document.createElement('td')
-				td.innerHTML = elem
-				td.addEventListener('click', function func () {
-					let input = document.createElement('input')	
-					input.value = td.innerHTML
-					td.innerHTML = ''
-					td.appendChild(input)
-					td.removeEventListener('click', func)
-					input.addEventListener('blur', function () {
-						td.innerHTML = input.value
-						td.addEventListener('click', func)
-					})
-				})
-				tr.appendChild(td)
-			}
-			let td = document.createElement('td')
-			let link = document.createElement('a')
-			link.innerHTML = 'Delete'
-			link.href = '#'
-			td.appendChild(link)
-			tr.appendChild(td)
-			link.addEventListener('click', function () {
-				table.removeChild(tr)
-			})
-			table.appendChild(tr)
-			this.total += this.price * this.quantity
-		}
-	}
+	
 });
 
