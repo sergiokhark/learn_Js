27c71ab5ca2409a855864adf388b96144ae9e8e3
sergiokhark <sergio.khark@gmail.com>
2022-07-16 14:05:16 +0300

    14.7_Vue

--- script.js
+++ script.js
@@ -13,21 +13,28 @@ let app = new Vue({
 			for (let elem of rest) {
 				let td = document.createElement('td')
 				td.innerHTML = elem
+				td.addEventListener('click', function func () {
+					let input = document.createElement('input')	
+					input.value = td.innerHTML
+					td.innerHTML = ''
+					td.appendChild(input)
+					td.removeEventListener('click', func)
+					input.addEventListener('blur', function () {
+						td.innerHTML = input.value
+						td.addEventListener('click', func)
+					})
+				})
 				tr.appendChild(td)
 			}
-
 			let td = document.createElement('td')
 			let link = document.createElement('a')
 			link.innerHTML = 'Delete'
 			link.href = '#'
 			td.appendChild(link)
 			tr.appendChild(td)
-			
 			link.addEventListener('click', function () {
 				table.removeChild(tr)
-				
 			})
-
 			table.appendChild(tr)
 			this.total += this.price * this.quantity
 		}
