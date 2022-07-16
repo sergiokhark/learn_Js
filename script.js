6e5237066ed951e7c0805e34bf8d74ce1dff1807
sergiokhark <sergio.khark@gmail.com>
2022-07-16 12:39:06 +0300

    14.4_Vue

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 let app = new Vue({
 	el: '#app',
 	data: {
-		users: [
-			{name: 'Коля', active: true},
-			{name: 'Вася', active: true},
-			{name: 'Петя', active: false},
+		products: [
+			{name: 'product1', price: 100, quantity: 4},
+			{name: 'product2', price: 200, quantity: 5},
+			{name: 'product3', price: 300, quantity: 6},
 		]
 	},
-	
+	methods: {
+		calc: function () {
+			let sum = 0
+			for (let product of this.products) {
+				sum += product.price * product.quantity
+			}
+			return sum
+		}
+	}
 });
 
