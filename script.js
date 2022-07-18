bc32147e522d421ace1e19258066be590150a40c
sergiokhark <sergio.khark@gmail.com>
2022-07-18 14:57:54 +0300

    15.3_Vue

--- script.js
+++ script.js
@@ -1,35 +1,17 @@
 let app = new Vue({
 	el: '#app',
 	data: {
-		products: [],
-		name: '',
-		price: '',
-		quantity: '',
-		total: null,
+		str: 'london is the capital of'
 	},
-	methods: {
-		addProduct: function () {
-			this.products.push({ name: this.name, price: this.price, quantity: this.quantity, edit: false })
-			this.getSum()
-		},
-		delProduct: function (index) {
-			this.products.splice(index, 1)	
-			this.getSum()
-		},
-		getSum: function () {
-			let sum = 0
-			for (let product of this.products) {
-				sum += product.price * product.quantity	
+	filters: {
+		capitalizeWords: function (str) {
+			let result = []
+			let arr = str.split(' ')
+			for (let elem of arr) {
+				result.push(elem[0].toUpperCase() + elem.slice(1))
 			}
-			this.total = sum
-		},
-		getEdit: function (index) {
-			this.products[index].edit = true
-			this.getSum()
-		},
-		hideEdit: function (index) {
-			this.products[index].edit = false
-			this.getSum()
+			return result.join(' ')
+		
 		}
 	}
 })
